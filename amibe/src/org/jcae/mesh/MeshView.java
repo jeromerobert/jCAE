3012ff7445fdf05b40ebcaa14b8c80302e5c0f50
barbier <barbier>
2004-11-25 14:39:59 +0000

    A main entry to display jcae3d meshes.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@15 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/MeshView.java
@@ -0,0 +1,69 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finit element mesher, Plugin architecture.
+
+    Copyright (C) 2003 Jerome Robert <jeromerobert@users.sourceforge.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
+package org.jcae.mesh;
+
+import java.io.File;
+import javax.media.j3d.BranchGroup;
+import org.jcae.mesh.xmldata.*;
+import org.jcae.mesh.java3d.*;
+import org.apache.log4j.Logger;
+
+/**
+ * This class MeshView allows to view a generated mesh.
+ */
+public class MeshView
+{
+	private static Logger logger=Logger.getLogger(MeshView.class);
+
+	public static void main(String args[])
+	{
+		if (args.length != 1)
+		{
+			System.out.println("Usage : MeshView directory");
+			System.exit(0);
+		}
+		String xmlDir = args[0];
+		try
+		{
+			String xmlFile = "jcae3d";
+			ComputeEdgesConnectivity computeEdgesConnectivity =
+				new ComputeEdgesConnectivity(xmlDir, xmlFile, "..");
+			computeEdgesConnectivity.compute();
+			
+			XMLBranchGroup xbg=new XMLBranchGroup(xmlDir, xmlFile);
+			xbg.parseXML();
+			Viewer v=new Viewer();
+			BranchGroup bg=xbg.getBranchGroup(0);
+			logger.info("Bounding sphere of the mesh: "+bg.getBounds());
+			v.addBranchGroup(bg);
+			bg=xbg.getBranchGroup("FreeEdges");
+			v.addBranchGroup(bg);
+			bg=xbg.getBranchGroup("MultiEdges");
+			v.addBranchGroup(bg);
+			v.show();
+			v.zoomTo();
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+}
