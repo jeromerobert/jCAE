83d5c93a20cf03e6a1ec33646e9d37f201892a30
barbier <barbier>
2005-01-28 10:29:13 +0000

    * Fix more errors with simultaneous reduction of metrics, deflection   should now work fine. * Move Matrix2D into its own class and do not herit from Matrix for   performance reasons.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@118 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/amibe/metrics/Matrix2D.java
@@ -0,0 +1,194 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finit element mesher, Plugin architecture.
+ 
+	Copyright (C) 2003 Jerome Robert <jeromerobert@users.sourceforge.net>
+ 
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+ 
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+ 
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jcae.mesh.amibe.metrics;
+
+import org.apache.log4j.Logger;
+
+public class Matrix2D
+{
+	private static Logger logger=Logger.getLogger(Matrix2D.class);
+	public double[][] data = new double[2][2];
+	
+	public Matrix2D()
+	{
+		data[0][0] = 1.0;
+		data[0][1] = 0.0;
+		data[1][0] = 0.0;
+		data[1][1] = 1.0;
+	}
+	public Matrix2D(double Axx, double Axy, double Ayx, double Ayy)
+	{
+		data[0][0] = Axx;
+		data[0][1] = Axy;
+		data[1][0] = Ayx;
+		data[1][1] = Ayy;
+	}
+	public double det()
+	{
+		return data[0][0] * data[1][1] - data[0][1] * data[1][0];
+	}
+	public Matrix2D inv()
+	{
+		double detA = det();
+		if (Math.abs(detA) < 1.e-10)
+			throw new RuntimeException("Singular matrice: "+this);
+		Matrix2D ret = new Matrix2D(data[1][1], -data[0][1], -data[1][0], data[0][0]);
+		ret.scale(1.0 / detA);
+		return ret;
+	}
+	public Matrix2D rotation(double theta)
+	{
+		double ct = Math.cos(theta);
+		double st = Math.sin(theta);
+		return new Matrix2D(ct, -st, st, ct);
+	}
+	public Matrix2D multR(Matrix2D A)
+	{
+		Matrix2D ret = new Matrix2D();
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+			{
+				ret.data[i][j] = 0.0;
+				for (int k = 0; k < 2; k++)
+					ret.data[i][j] += data[i][k] * A.data[k][j];
+			}
+		return ret;
+	}
+	public double [] apply(double [] in)
+	{
+		double [] out = new double[2];
+		out[0] = data[0][0] * in[0] + data[0][1] * in[1];
+		out[1] = data[1][0] * in[0] + data[1][1] * in[1];
+		return out;
+	}
+	public double norm2(double vx, double vy)
+	{
+		return data[0][0] * vx * vx + (data[1][0] + data[0][1]) * vx * vy + data[1][1] * vy * vy;
+	}
+	public void scale(double f)
+	{
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				data[i][j] *= f;
+	}
+	
+	
+	/**
+	 *  Compute eigenvectors of a positive matrix
+	 */
+	private Matrix2D eigenvectors ()
+	{
+		Matrix2D ret = new Matrix2D();
+		double delta = (data[0][0] - data[1][1]) * (data[0][0] - data[1][1]) + 4.0 * data[0][1] * data[1][0];
+		double l1 = 0.5 * (data[0][0] + data[1][1] + Math.sqrt(delta));
+		double l2 = 0.5 * (data[0][0] + data[1][1] - Math.sqrt(delta));
+		double t1 = (data[0][0]-l1) * (data[0][0]-l1);
+		double t2 = data[0][1] * data[0][1];
+		double t3 = data[1][0] * data[1][0];
+		double t4 = (data[1][1]-l1) * (data[1][1]-l1);
+		if (t1 + t2 < t3 + t4)
+		{
+			double invnorm = 1.0 / Math.sqrt(t3 + t4);
+			ret.data[0][0] = (l1-data[1][1]) * invnorm;
+			ret.data[1][0] = data[1][0] * invnorm;
+		}
+		else
+		{
+			double invnorm = 1.0 / Math.sqrt(t1 + t2);
+			ret.data[0][0] = data[0][1] * invnorm;
+			ret.data[1][0] = (l1-data[0][0]) * invnorm;
+		}
+		t1 = (data[0][0]-l2) * (data[0][0]-l2);
+		t2 = data[0][1] * data[0][1];
+		t4 = data[1][0] * data[1][0];
+		t4 = (data[1][1]-l2) * (data[1][1]-l2);
+		if (t1 + t2 < t3 + t4)
+		{
+			double invnorm = 1.0 / Math.sqrt(t3 + t4);
+			ret.data[0][1] = (l2-data[1][1]) * invnorm;
+			ret.data[1][1] = data[1][0] * invnorm;
+		}
+		else
+		{
+			double invnorm = 1.0 / Math.sqrt(t1 + t2);
+			ret.data[0][1] = data[0][1] * invnorm;
+			ret.data[1][1] = (l2-data[0][0]) * invnorm;
+		}
+		return ret;
+	}
+	
+	/**
+	 *  Computes the simultaneous reduction of 2 metrics
+	 */
+	private Matrix2D simultaneousReduction (Matrix2D B)
+	{
+		Matrix2D AinvB = inv().multR(B);
+		double delta = (AinvB.data[0][0] - AinvB.data[1][1]) * (AinvB.data[0][0] - AinvB.data[1][1]) + 4.0 * AinvB.data[0][1] * AinvB.data[1][0];
+		if (delta < 1.e-20)
+			//  Matrices are similar.  Return eigenvectors of A
+			return eigenvectors();
+		
+		return AinvB.eigenvectors();
+	}
+	
+	/**
+	 *  Computes the intersection of 2 metrics.
+	 */
+	public Matrix2D intersection (Matrix2D B)
+	{
+		Matrix2D res = simultaneousReduction(B);
+		Matrix2D resInv = res.inv();
+		double ev1 = Math.max(
+			norm2(res.data[0][0], res.data[1][0]),
+			B.norm2(res.data[0][0], res.data[1][0])
+		);
+		double ev2 = Math.max(
+			norm2(res.data[0][1], res.data[1][1]),
+			B.norm2(res.data[0][1], res.data[1][1])
+		);
+		Matrix2D D = new Matrix2D(ev1, 0.0, 0.0, ev2);
+		double a11 = ev1 * resInv.data[0][0] * resInv.data[0][0] + ev2 * resInv.data[1][0] * resInv.data[1][0];
+		double a21 = ev1 * resInv.data[0][0] * resInv.data[0][1] + ev2 * resInv.data[1][0] * resInv.data[1][1];
+		double a22 = ev1 * resInv.data[0][1] * resInv.data[0][1] + ev2 * resInv.data[1][1] * resInv.data[1][1];
+		return new Matrix2D(a11, a21, a21, a22);
+	}
+	public String toString()
+	{
+		return "Matrix2D: ("+data[0][0]+", "+data[0][1]+", "+data[1][0]+", "+data[1][1]+")";
+	}
+	
+	/*  Unit tests.  */
+	public static void main(String args[])
+	{
+		Matrix2D A, B, C, D;
+		A = new Matrix2D(4.0, 1.0, 1.0, 1.0);
+		System.out.println("A: "+A);
+		System.out.println("inv A: "+A.inv());
+		A = new Matrix2D(4.0, 0.0, 0.0, 2.0);
+		B = new Matrix2D(2.0, 0.0, 0.0, 1.0);
+		System.out.println("A: "+A);
+		System.out.println("B: "+B);
+		C = B.intersection(A);
+		System.out.println("C: "+C);
+		C = A.intersection(B);
+		System.out.println("C: "+C);
+	}
+}
--- amibe/src/org/jcae/mesh/amibe/metrics/Metric2D.java
+++ amibe/src/org/jcae/mesh/amibe/metrics/Metric2D.java
@@ -40,168 +40,6 @@ public class Metric2D
 	// Static array to speed up orth() method
 	private static double [] orthRes = new double[2];
 
-	private class Matrix2D extends Matrix
-	{
-		public Matrix2D()
-		{
-			rank = 2;
-			data = new double[rank][rank];
-			data[0][0] = 1.0;
-			data[0][1] = 0.0;
-			data[1][0] = 0.0;
-			data[1][1] = 1.0;
-		}
-		public Matrix2D(double Axx, double Axy, double Ayx, double Ayy)
-		{
-			rank = 2;
-			data = new double[rank][rank];
-			data[0][0] = Axx;
-			data[0][1] = Axy;
-			data[1][0] = Ayx;
-			data[1][1] = Ayy;
-		}
-		public double det()
-		{
-			return data[0][0] * data[1][1] - data[0][1] * data[1][0];
-		}
-		public Matrix2D inv()
-		{
-			double detA = det();
-			if (Math.abs(detA) < 1.e-10)
-				throw new RuntimeException("Singular matrice: "+this);
-			Matrix2D ret = new Matrix2D(data[1][1], -data[1][0], -data[0][1], data[0][0]);
-			ret.scale(1.0 / detA);
-			return ret;
-		}
-		public Matrix2D rotation(double theta)
-		{
-			double ct = Math.cos(theta);
-			double st = Math.sin(theta);
-			return new Matrix2D(ct, -st, st, ct);
-		}
-		public double [] apply(double [] in)
-		{
-			double [] out = new double[2];
-			out[0] = data[0][0] * in[0] + data[0][1] * in[1];
-			out[1] = data[1][0] * in[0] + data[1][1] * in[1];
-			return out;
-		}
-		public double norm2(double vx, double vy)
-		{
-			return data[0][0] * vx * vx + (data[1][0] + data[0][1]) * vx * vy + data[1][1] * vy * vy;
-		}
-		
-		/**
-		 *  Computes the simultaneous reduction of 2 metrics
-		 */
-		private Matrix2D simultaneousReduction (Matrix2D B)
-		{
-			Matrix2D ret = new Matrix2D();
-			// det(A - l B) = (a11 - l b11)*(a22 - l b22) - (a21 - l b21)^2
-			//  = detB l^2 - l (a11*b22+a22*b11-2*a21*b21) + detA
-			//  = a l^2 - b l + c
-			// Delta = (a11*b22+a22*b11-2*a21*b21)^2 - 4 detA detB
-			double a = B.det();
-			double b = data[0][0]*B.data[1][1] + data[1][1]*B.data[0][0] - 2.0 * data[1][0]*B.data[1][0];
-			double c = det();
-			double delta = b*b - 4.0 * a * c;
-			if (a < 1.e-10 * b)
-			{
-				ret.data[0][0] = ret.data[1][1] = 1.0;
-				ret.data[0][1] = ret.data[1][0] = 0.0;
-				return ret;
-			}
-			else if (delta < 1.e-4 * Math.abs(a*b))
-			{
-				//  B is similar to A.  Search for eigenvectors of A
-				delta = (data[0][0] - data[1][1]) * (data[0][0] - data[1][1]) + 4.0 * data[0][1] * data[0][1];
-				double l1 = 0.5 * (data[0][0] + data[1][1] + Math.sqrt(delta));
-				double l2 = 0.5 * (data[0][0] + data[1][1] - Math.sqrt(delta));
-				double invnorm1 = 1.0 / Math.sqrt((data[0][0]-l1) * (data[0][0]-l1) + data[1][0] * data[1][0]);
-				ret.data[0][0] = -data[1][0] * invnorm1;
-				ret.data[1][0] = (data[0][0]-l1) * invnorm1;
-				ret.data[0][1] = -ret.data[1][0];
-				ret.data[1][1] = ret.data[0][0];
-				return ret;
-			}
-			
-			double l1 = 0.5 * (b + Math.sqrt(delta)) / a;
-			double l2 = 0.5 * (b - Math.sqrt(delta)) / a;
-			/*  Now solve
-			 *   (A -l1 B) V1 = 0
-			 *  An eigenvector is colinear to
-			 *    ( a21-l1*B.a21, -a11+l1 B.a11) and
-			 *    ( a22-l1*B.a22, -a21+l1*B.a21)
-			 *  and the other one is orthogonal.
-			 */
-	
-			double t1 = (data[0][0]-l1*B.data[0][0]) * (data[0][0]-l1*B.data[0][0]);
-			double t2 = (data[1][0]-l1*B.data[1][0]) * (data[1][0]-l1*B.data[1][0]);
-			double t3 = (data[1][1]-l1*B.data[1][1]) * (data[1][1]-l1*B.data[1][1]);
-			if (t1 < t3)
-			{
-				double invnorm = 1.0 / Math.sqrt(t2 + t3);
-				ret.data[0][0] = (data[1][1]-l1*B.data[1][1]) * invnorm;
-				ret.data[1][0] = (-data[1][0]+l1*B.data[1][0]) * invnorm;
-			}
-			else
-			{
-				double invnorm = 1.0 / Math.sqrt(t2 + t1);
-				ret.data[0][0] = (data[1][0]-l1*B.data[1][0]) * invnorm;
-				ret.data[1][0] = (-data[0][0]+l1*B.data[0][0]) * invnorm;
-			}
-			if (delta == 0.0)
-			{
-				ret.data[0][1] = - ret.data[1][0];
-				ret.data[1][1] = ret.data[0][0];
-				return ret;
-			}
-			
-			t1 = (data[0][0]-l2*B.data[0][0]) * (data[0][0]-l2*B.data[0][0]);
-			t2 = (data[1][0]-l2*B.data[1][0]) * (data[1][0]-l2*B.data[1][0]);
-			t3 = (data[1][1]-l2*B.data[1][1]) * (data[1][1]-l2*B.data[1][1]);
-			if (t1 < t3)
-			{
-				double invnorm = 1.0 / Math.sqrt(t2 + t3);
-				ret.data[0][1] = (data[1][1]-l2*B.data[1][1]) * invnorm;
-				ret.data[1][1] = (-data[1][0]+l2*B.data[1][0]) * invnorm;
-			}
-			else
-			{
-				double invnorm = 1.0 / Math.sqrt(t2 + t1);
-				ret.data[0][1] = (data[1][0]-l2*B.data[1][0]) * invnorm;
-				ret.data[1][1] = (-data[0][0]+l2*B.data[0][0]) * invnorm;
-			}
-			return ret;
-		}
-		
-		/**
-		 *  Computes the intersection of 2 metrics.
-		 */
-		public Matrix2D intersection (Matrix2D B)
-		{
-			Matrix2D res = simultaneousReduction(B);
-			Matrix2D resInv = res.inv();
-			double ev1 = Math.max(
-				norm2(res.data[0][0], res.data[1][0]),
-				B.norm2(res.data[0][0], res.data[1][0])
-			);
-			double ev2 = Math.max(
-				norm2(res.data[0][1], res.data[1][1]),
-				B.norm2(res.data[0][1], res.data[1][1])
-			);
-			Matrix2D D = new Matrix2D(ev1, 0.0, 0.0, ev2);
-			double a11 = ev1 * resInv.data[0][0] * resInv.data[0][0] + ev2 * resInv.data[1][0] * resInv.data[1][0];
-			double a21 = ev1 * resInv.data[0][0] * resInv.data[0][1] + ev2 * resInv.data[1][0] * resInv.data[1][1];
-			double a22 = ev1 * resInv.data[0][1] * resInv.data[0][1] + ev2 * resInv.data[1][1] * resInv.data[1][1];
-			return new Matrix2D(a11, a21, a21, a22);
-		}
-		public String toString()
-		{
-			return "Matric2D: ("+data[0][0]+", "+data[0][1]+", "+data[1][0]+", "+data[1][1]+")";
-		}
-	}
-	
 	/**
 	 * Creates a <code>Metric2D</code> instance at a given point.
 	 *
@@ -235,8 +73,7 @@ public class Metric2D
 				Matrix2D res = m2d0.intersection(m2d1);
 				temp[0][0] = res.data[0][0];
 				temp[1][1] = res.data[1][1];
-				//  FIXME:  Why is -1 needed?
-				sym = -0.5 * (res.data[0][1] + res.data[1][0]);
+				sym = 0.5 * (res.data[0][1] + res.data[1][0]);
 			}
 		}
 		E = temp[0][0];
@@ -432,4 +269,5 @@ public class Metric2D
 	{
 		return "Metric2D: E="+E+" F="+F+" G="+G;
 	}
+	
 }
