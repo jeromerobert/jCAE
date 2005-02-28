f63d2774b70371392e140182f43750a8366e22e2
barbier <barbier>
2005-02-28 15:18:38 +0000

    Cosmetics
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@158 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/cad/CADGeomSurface.java
+++ amibe/src/org/jcae/mesh/cad/CADGeomSurface.java
@@ -24,54 +24,59 @@ package org.jcae.mesh.cad;
  */
 public interface CADGeomSurface
 {
-    /**
-     * Initialize the degree of the surface
-     * @param degree The degree of the surface
-     */    
+	/**
+	 * Initialize the degree of the surface
+	 * @param degree The degree of the surface
+	 */    
 	public void dinit(int degree);
     
 	/**
-     * Set the u, v coordinates used for d1U, d1V and curvature operation
-     * @param u The u coordinate
-     * @param v The v coordinate
-     */    
+	 * Set the u, v coordinates used for d1U, d1V and curvature operation
+	 * @param u The u coordinate
+	 * @param v The v coordinate
+	 */    
 	public void setParameter(double u, double v);
     
 	/**
-     * Return the u first derivative vector at the coordinates set by setParameter method
-     * @return the u first derivative
-     */    
+	 * Return the u first derivative vector at the coordinates set by setParameter method
+	 * @return the u first derivative
+	 */    
 	public double [] d1U();
     
 	/**
-     * Return the v first derivative vector at the coordinates set by setParameter method
-     * @return the v first derivative
-     */    
+	 * Return the v first derivative vector at the coordinates set by setParameter method
+	 * @return the v first derivative
+	 */    
 	public double [] d1V();
 	public double [] d2U();
 	public double [] d2V();
 	public double [] dUV();
+
+	/**
+	 * Return the normal to the surface 
+	 */    
+	public double [] normal();
     
 	/**
-     * Get 3D coordinates from (u, v) coordinates
-     * @param u The u coordinate
-     * @param v The v coordinate
-     * @return A array {x, y, z}
-     */    
+	 * Get 3D coordinates from (u, v) coordinates
+	 * @param u The u coordinate
+	 * @param v The v coordinate
+	 * @return A array {x, y, z}
+	 */    
 	public double [] value(double u, double v);
-    /**
-     * Return the minimum curvature at the current point
-     * @return the minimum curvature at the current point
-     */    
+	/**
+	 * Return the minimum curvature at the current point
+	 * @return the minimum curvature at the current point
+	 */    
 	public double minCurvature();
-    /**
-     * Return the maximum curvature at the current point
-     * @return The maximum curvature at the current point
-     */    
+	/**
+	 * Return the maximum curvature at the current point
+	 * @return The maximum curvature at the current point
+	 */    
 	public double maxCurvature();
-    /**
-     * Return the direction of maximum and minimum curvature at the current point
-     * @return An array: {Xmax, Ymax, Zmax, Xmin, Ymin, Zmin}
-     */    
+	/**
+	 * Return the direction of maximum and minimum curvature at the current point
+	 * @return An array: {Xmax, Ymax, Zmax, Xmin, Ymin, Zmin}
+	 */    
 	public double [] curvatureDirections();
 }
