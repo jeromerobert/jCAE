8d4e1578fb5adb09fdbef9c603d90f6e2ca6f300
barbier <barbier>
2005-08-09 12:21:45 +0000

    New class, to handle edges when orientation is not needed.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@462 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/amibe/ds/NotOrientedEdge.java
@@ -0,0 +1,50 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finit element mesher, Plugin architecture.
+
+    Copyright (C) 2004,2005
+                  Jerome Robert <jeromerobert@users.sourceforge.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+package org.jcae.mesh.amibe.ds;
+
+public class NotOrientedEdge extends OTriangle
+{
+	public NotOrientedEdge()
+	{
+		super();
+	}
+	
+	public NotOrientedEdge(OTriangle ot)
+	{
+		tri = ot.tri;
+		orientation = ot.orientation;
+		attributes = (tri.adjPos >> (8*(1+orientation))) & 0xff;
+	}
+
+	public final int hashCode()
+	{
+		return tri.hashCode() + tri.adj[orientation].hashCode();
+	}
+	
+	public final boolean equals(Object o)
+	{
+		if (o == null)
+			return false;
+		NotOrientedEdge that = (NotOrientedEdge) o;
+		return (that.tri == tri && that.orientation == orientation) ||
+		       (that.tri == tri.adj[orientation] && that.orientation == ((tri.adjPos >> (2*orientation)) & 3));
+	}
+}
