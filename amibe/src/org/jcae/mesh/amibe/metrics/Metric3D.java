5ce49eb2ee89e4bbbf940e89ee6be113a708c60c
barbier <barbier>
2005-08-10 13:50:40 +0000

    New prodVect3D() method, works on an already allocated array
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@471 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/amibe/metrics/Metric3D.java
+++ amibe/src/org/jcae/mesh/amibe/metrics/Metric3D.java
@@ -95,10 +95,15 @@ public class Metric3D extends Matrix
 	public static double [] prodVect3D(double [] v1, double [] v2)
 	{
 		double [] ret = new double[3];
+		prodVect3D(v1, v2, ret);
+		return ret;			
+	}
+	
+	public static void prodVect3D(double [] v1, double [] v2, double [] ret)
+	{
 		ret[0] = v1[1] * v2[2] - v1[2] * v2[1];
 		ret[1] = v1[2] * v2[0] - v1[0] * v2[2];
 		ret[2] = v1[0] * v2[1] - v1[1] * v2[0];
-		return ret;			
 	}
 	
 	public boolean iso(double l)
