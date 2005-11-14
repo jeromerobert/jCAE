083927672eccc199236a1bf343b7c71e8a5f7e7a
barbier <barbier>
2005-11-14 15:43:03 +0000

    Add an optional argument to compute() to select vertex placement strategy.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@575 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/amibe/algos3d/DecimateVertex.java
+++ amibe/src/org/jcae/mesh/amibe/algos3d/DecimateVertex.java
@@ -42,6 +42,11 @@ public class DecimateVertex
 	private static Logger logger=Logger.getLogger(DecimateVertex.class);
 	private Mesh mesh;
 	private double tolerance;
+	private int placement;
+	public static final int POS_VERTEX = 0;
+	public static final int POS_MIDDLE = 1;
+	public static final int POS_EDGE = 2;
+	public static final int POS_OPTIMAL = 3;
 	
 	private class Quadric
 	{
@@ -89,6 +94,21 @@ public class DecimateVertex
 	{
 		mesh = m;
 		tolerance = tol;
+		placement = POS_EDGE;
+	}
+	
+	/**
+	 * Creates a <code>DecimateVertex</code> instance.
+	 *
+	 * @param m  the <code>Mesh</code> instance to refine.
+	 * @param tol  maximal allowed error
+	 * @param p  placement of the new point. Legitimate values are <code>POS_VERTEX</code> (at a vertex location), <code>POS_MIDDLE</code> (at the middle of the contracted edge), <code>POS_EDGE</code> (opyimal placement on the contracted edge, this is the default) and <code>POS_OPTIMAL</code> (optimal placement).)
+	 */
+	public DecimateVertex(Mesh m, double tol, int p)
+	{
+		mesh = m;
+		tolerance = tol;
+		placement = p;
 	}
 	
 	/**
@@ -257,14 +277,14 @@ public class DecimateVertex
 				q3.add(q1);
 				q3.add(q2);
 				v3 = optimalPlacement(v1, v2, q1, q2, q3);
-				if (edge.canContract(v3))
+				if (edge.isMutable() && edge.canContract(v3))
 					break;
 				if (logger.isDebugEnabled())
 					logger.info("Edge not contracted: "+edge);
 				edge = (NotOrientedEdge) tree.next();
 				cost = tree.getKey(edge);
 			} while (edge != null && cost <= tolerance);
-			if (cost > tolerance)
+			if (cost > tolerance || edge == null)
 				break;
 			tree.remove(edge);
 			if (logger.isDebugEnabled())
@@ -354,26 +374,75 @@ public class DecimateVertex
 		return ret;
 	}
 	
-	private static Vertex optimalPlacement(Vertex v1, Vertex v2, Quadric q1, Quadric q2, Quadric q3)
+	private Vertex optimalPlacement(Vertex v1, Vertex v2, Quadric q1, Quadric q2, Quadric q3)
 	{
 		Vertex ret;
-		Metric3D Qinv = q3.A.inv();
-		if (Qinv != null)
+		assert v1 != Vertex.outer;
+		assert v2 != Vertex.outer;
+		if (placement == POS_VERTEX)
+		{
+			if (q1.value(v2.getUV()) + q2.value(v2.getUV()) < q1.value(v1.getUV()) + q2.value(v1.getUV()))
+				ret = (Vertex) v2.clone();
+			else
+				ret = (Vertex) v1.clone();
+		}
+		else if (placement == POS_MIDDLE)
 		{
 			// Keep a reference if there is one
 			if (v1.getRef() != 0)
 				ret = (Vertex) v1.clone();
 			else
 				ret = (Vertex) v2.clone();
-			double [] dx = Qinv.apply(q3.b);
-			ret.moveTo(-dx[0], -dx[1], -dx[2]);
+			double [] p1 = v1.getUV();
+			double [] p2 = v2.getUV();
+			ret.moveTo(0.5*(p1[0]+p2[0]), 0.5*(p1[1]+p2[1]), 0.5*(p1[2]+p2[2]));
 		}
 		else
 		{
-			if (q1.value(v2.getUV()) + q2.value(v2.getUV()) < q1.value(v1.getUV()) + q2.value(v1.getUV()))
-				ret = (Vertex) v2.clone();
+			// POS_EDGE and POS_OPTIMAL
+			Metric3D Qinv = q3.A.inv();
+			if (Qinv != null)
+			{
+				// Keep a reference if there is one
+				if (v1.getRef() != 0)
+					ret = (Vertex) v1.clone();
+				else
+					ret = (Vertex) v2.clone();
+				double [] dx = Qinv.apply(q3.b);
+				if (placement == POS_OPTIMAL)
+					ret.moveTo(-dx[0], -dx[1], -dx[2]);
+				else
+				{
+					assert placement == POS_EDGE;
+					// Project ret onto [v1;v2]
+					double [] vect1 = new double[3];
+					double [] vect2 = new double[3];
+					double [] p1 = v1.getUV();
+					double [] p2 = v2.getUV();
+					for (int i = 0; i < 3; i++)
+					{
+						vect1[i] = p2[i] - p1[i];
+						vect2[i] = -dx[i] - p1[i];
+					}
+					double l = Metric3D.norm(vect1);
+					if (l > 1.e-20)
+						for (int i = 0; i < 3; i++)
+							vect1[i] /= l;
+					double s = Metric3D.prodSca(vect1, vect2);
+					if (s < 0.0)
+						s = 0.0;
+					else if (s > l)
+						s = l;
+					ret.moveTo(p1[0]+s*vect1[0]/l, p1[1]+s*vect1[1]/l, p1[2]+s*vect1[2]/l);
+				}
+			}
 			else
-				ret = (Vertex) v1.clone();
+			{
+				if (q1.value(v2.getUV()) + q2.value(v2.getUV()) < q1.value(v1.getUV()) + q2.value(v1.getUV()))
+					ret = (Vertex) v2.clone();
+				else
+					ret = (Vertex) v1.clone();
+			}
 		}
 		return ret;
 	}
