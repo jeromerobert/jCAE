0d9a6a4d26c9c109534772c033c1899a549137d8
barbier <barbier>
2006-11-13 20:06:02 +0000

    New CAD entities.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@808 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src-occ/org/jcae/mesh/cad/occ/OCCIterator.java
@@ -0,0 +1,56 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finit element mesher, Plugin architecture.
+
+    Copyright (C) 2004,2005
+                  Jerome Robert <jeromerobert@users.sourceforge.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+package org.jcae.mesh.cad.occ;
+
+import org.jcae.mesh.cad.CADIterator;
+import org.jcae.mesh.cad.CADShape;
+import org.jcae.opencascade.jni.TopoDS_Iterator;
+import org.jcae.opencascade.jni.TopoDS_Shape;
+
+public class OCCIterator implements CADIterator
+{
+	private TopoDS_Iterator occIt;
+	public OCCIterator ()
+	{
+		occIt = new TopoDS_Iterator();
+	}
+	
+	public void initialize(CADShape s)
+	{
+		OCCShape shape = (OCCShape) s;
+		occIt.initialize((TopoDS_Shape) shape.getShape());
+	}
+	
+	public boolean more()
+	{
+		return occIt.more();
+	}
+	
+	public void next()
+	{
+		occIt.next();
+	}
+	
+	public CADShape value()
+	{
+		return OCCShapeBuilder.factory.newShape(occIt.value());
+	}
+}
---
+++ amibe/src/org/jcae/mesh/cad/CADIterator.java
@@ -0,0 +1,48 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finit element mesher, Plugin architecture.
+
+    Copyright (C) 2006, by EADS CRC
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+package org.jcae.mesh.cad;
+
+/**
+ * Iterator on sub-shapes.
+ * @author Denis Barbier
+ *
+ */
+public interface CADIterator
+{
+    /**
+     * Initialize this explorer
+     * @param shape The shape to explore
+     */    
+	public void initialize(CADShape shape);
+    /**
+     * Return true if there are more shapes to explore
+     * @return true if there are more shapes to explore
+     */    
+	public boolean more();
+    /**
+     * Move on to the next shape in the exploration
+     */    
+	public void next();
+    /**
+     * Return the current shape in the exploration
+     * @return the current shape in the exploration
+     */    
+	public CADShape value();
+}
