5cf387cf38dde69e59df2466114962035ce0624e
barbier <barbier>
2007-02-07 16:00:27 +0000

    Two more programs to play with OEMM stuff
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1055 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/MeshOEMMDecimate.java
@@ -0,0 +1,77 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finite element mesher, Plugin architecture.
+
+    Copyright (C) 2007, by EADS France
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
+package org.jcae.mesh;
+
+import org.jcae.mesh.amibe.ds.Mesh;
+import org.jcae.mesh.oemm.OEMM;
+import org.jcae.mesh.oemm.OEMMNode;
+import org.jcae.mesh.oemm.IndexedStorage;
+import org.jcae.mesh.oemm.RawStorage;
+import org.jcae.mesh.oemm.TraversalProcedure;
+import java.util.HashMap;
+import gnu.trove.TIntHashSet;
+
+/**
+ * Decimates all cells of an OEMM.
+ */
+public class MeshOEMMDecimate
+{
+
+	public static void main(String args[])
+	{
+		if (args.length < 2)
+		{
+			System.out.println("Usage: MeshOEMMDecimate oemm scaleTriangles");
+			System.exit(0);
+		}
+		String dir = args[0];
+		int scale = Integer.valueOf(args[1]).intValue();
+		
+		OEMM oemm = IndexedStorage.buildOEMMStructure(dir);
+		DecimateProcedure d_proc = new DecimateProcedure(oemm, scale);
+		oemm.walk(d_proc);
+		// TODO: write mesh back into oemm
+	}
+
+	private static class DecimateProcedure extends TraversalProcedure
+	{
+		private OEMM oemm;
+		private int scale;
+		public DecimateProcedure(OEMM o, int s)
+		{
+			oemm = o;
+			scale = s;
+		}
+		public final int action(OEMMNode current, int octant, int visit)
+		{
+			if (visit != LEAF)
+				return SKIPWALK;
+			TIntHashSet leaves = new TIntHashSet();
+			leaves.add(current.leafIndex);
+			Mesh amesh = IndexedStorage.loadNodes(oemm, leaves);
+			HashMap options = new HashMap();
+			options.put("maxtriangles", ""+(current.tn / scale));
+			new org.jcae.mesh.amibe.algos3d.DecimateHalfEdge(amesh, options).compute();
+			return OK;
+		}
+	}
+
+}
---
+++ amibe/src/org/jcae/mesh/MeshOEMMPopulate.java
@@ -0,0 +1,50 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finite element mesher, Plugin architecture.
+
+    Copyright (C) 2007, by EADS France
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+
+package org.jcae.mesh;
+
+import org.jcae.mesh.oemm.OEMM;
+import org.jcae.mesh.oemm.IndexedStorage;
+import org.jcae.mesh.oemm.RawStorage;
+
+/**
+ * This class fills an OEMM structure with a new triangle soup.
+ */
+public class MeshOEMMPopulate
+{
+	public static void main(String args[])
+	{
+		if (args.length < 3)
+		{
+			System.out.println("Usage: MeshOEMMPopulate inOEMM outOEMM soup");
+			System.exit(0);
+		}
+		String inOEMM   = args[0];
+		String outOEMM  = args[1];
+		String soupFile = args[2];
+		// Read initial OEMM structure
+		OEMM oemm = IndexedStorage.buildOEMMStructure(inOEMM);
+		// Convert triangle soup into an intermediate OEMM file
+		RawStorage.dispatch(oemm, soupFile, "dispatched", "dispatched.data");
+		// Convert intermediate OEMM file into final OEMM
+		IndexedStorage.indexOEMM("dispatched", outOEMM);
+	}
+
+}
