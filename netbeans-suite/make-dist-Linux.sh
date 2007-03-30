2aa5456289c6630265b5635a84b9086e32b088db
jeromerobert <jeromerobert>
2007-03-30 14:54:32 +0000

    Add distribution building scripts
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1152 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ netbeans-suite/make-dist-Linux.sh
@@ -0,0 +1,36 @@
+#! /bin/sh -x
+
+JAVA_NAME=jre-6
+VERSION=jcae-0.14
+rm -rf $VERSION
+mkdir $VERSION
+cd $VERSION
+
+cat > jcae.sh <<EOF
+#! /bin/sh
+MEMORY=512m
+ZFACTORABS=20
+ZFACTORREL=2
+MMGT_OPT=0
+export MMGT_OPT
+jcaeHome=\`dirname \$0\`
+LD_LIBRARY_PATH=\$jcaeHome/OpenCASCADE6.2.0/ros/Linux/lib:\$jcaeHome/jcae/modules/ext/:\$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+\$jcaeHome/bin/jcae --jdkhome \$jcaeHome/$JAVA_NAME -J-Xmx\$MEMORY -J-Djavax.media.j3d.zFactorAbs=\$ZFACTORABS -J-Djavax.media.j3d.zFactorRel=\$ZFACTORREL
+EOF
+
+chmod +x jcae.sh
+
+cp -r /usr/lib/jvm/java-6-sun/jre/ $JAVA_NAME
+rm -rf $JAVA_NAME/plugin $JAVA_NAME/javaw $JAVA_NAME/lib/i386/client/classes.jsa
+mkdir -p OpenCASCADE6.2.0/ros/Linux/
+ln -s $CASROOT/../LICENSE OpenCASCADE6.2.0/LICENSE
+ln -s $CASROOT/Linux-jcae/lib OpenCASCADE6.2.0/ros/Linux/lib
+unzip ../dist/jcae.zip
+mv jcae tmp
+mv tmp/* .
+rmdir tmp
+
+cd ..
+tar cvfj $VERSION-Linux.tar.bz2 --totals --owner 0 --group 0 -h $VERSION
+
---
+++ netbeans-suite/make-dist-Win32.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+JAVA_NAME=jre-6
+VERSION=jcae-0.14
+rm -rf $VERSION
+mkdir $VERSION
+cd $VERSION
+
+cat > jcae.bat <<EOF
+set MEMORY=512m
+set ZFACTORABS=0
+set ZFACTORREL=0
+set MMGT_OPT=0
+set jcaeHome=%~dp0
+set PATH=%jcaeHome%OpenCASCADE6.2.0/ros/win32/bin;%jcaeHome%jcae/modules/ext/;%PATH%
+"%jcaeHome%/bin/jcae" --jdkhome "%jcaeHome%/$JAVA_NAME" -J-Xmx%MEMORY% -J-Djavax.media.j3d.zFactorAbs=%ZFACTORABS% -J-Djavax.media.j3d.zFactorRel=%ZFACTORREL%
+EOF
+
+ln -s ../jre-6-win32/ $JAVA_NAME
+mkdir -p OpenCASCADE6.2.0/ros/win32
+ln -s $CASROOT/../LICENSE OpenCASCADE6.2.0/LICENSE
+ln -s $CASROOT/win32-jcae/bin/ OpenCASCADE6.2.0/ros/win32/bin
+unzip ../dist/jcae.zip
+mv jcae tmp
+mv tmp/* .
+rmdir tmp
+
+cd ..
+
+zip -9r "$VERSION-win32.zip" "$VERSION"
+
