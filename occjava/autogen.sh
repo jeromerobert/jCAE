e53cc9afc8785289c3b8088d28e5cbd51649ed2b
barbier <barbier>
2007-04-10 09:12:08 +0000

    * autogen.sh: Fix bashism, patch by nyholku https://sourceforge.net/forum/message.php?msg_id=4249115
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1167 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- occjava/autogen.sh
+++ occjava/autogen.sh
@@ -15,8 +15,8 @@ set -e
 
 # Refresh GNU autotools toolchain.
 echo Cleaning autotools files...
-find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
-find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
+find . -type d -name autom4te.cache -print0 | xargs -0 rm -rf
+find . -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
 	-o -name depcomp -o -name ltmain.sh -o -name configure \
 	-o -name config.sub -o -name config.guess \
 	-o -name Makefile.in \) -print0 | xargs -0 rm -f
