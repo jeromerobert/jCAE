b425b7de15a2a658af3d82e34a5abf80faf48d05
jeromerobert <jeromerobert>
2007-05-04 09:15:07 +0000

    Fix typos
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1206 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- occjava/src-java/org/jcae/opencascade/jni/BRepOffsetAPI_Sewing.java
+++ occjava/src-java/org/jcae/opencascade/jni/BRepOffsetAPI_Sewing.java
@@ -2,40 +2,40 @@ package org.jcae.opencascade.jni;
 
 /**
  * This class seems be deprecated in Opencascade 6.2
- * @deprected use BRepBuilderAPI_Sewing
+ * @deprecated use BRepBuilderAPI_Sewing
  */
 public class BRepOffsetAPI_Sewing extends BRepBuilderAPI_Sewing
 {
 	/**
-	 * @deprected use BRepBuilderAPI_Sewing
+	 * @deprecated use BRepBuilderAPI_Sewing
 	 */
 	public BRepOffsetAPI_Sewing(double tolerance, boolean option, boolean cutting, boolean nonmanifold) {
 		super(tolerance, option, cutting, nonmanifold);
 	}
 
 	/**
-	 * @deprected use BRepBuilderAPI_Sewing
+	 * @deprecated use BRepBuilderAPI_Sewing
 	 */
 	public BRepOffsetAPI_Sewing(double tolerance, boolean option, boolean cutting) {
 		super(tolerance, option, cutting);
 	}
 
 	/**
-	 * @deprected use BRepBuilderAPI_Sewing
+	 * @deprecated use BRepBuilderAPI_Sewing
 	 */
 	public BRepOffsetAPI_Sewing(double tolerance, boolean option) {
 		super(tolerance, option);
 	}
 
 	/**
-	 * @deprected use BRepBuilderAPI_Sewing
+	 * @deprecated use BRepBuilderAPI_Sewing
 	 */
 	public BRepOffsetAPI_Sewing(double tolerance) {
 		super(tolerance);
 	}
 
 	/**
-	 * @deprected use BRepBuilderAPI_Sewing
+	 * @deprecated use BRepBuilderAPI_Sewing
 	 */
 	public BRepOffsetAPI_Sewing() {
 	}
