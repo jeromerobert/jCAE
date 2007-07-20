ae2c6b09f1ca0d183825198293d5e780bb7d7d93
barbier <barbier>
2007-07-20 09:59:34 +0000

    Revert UniformLengthDeflection1d.java back to r1240, r1307 has been copied into AlgoMesh1d.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1361 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/bora/algo/UniformLengthDeflection1d.java
+++ amibe/src/org/jcae/mesh/bora/algo/UniformLengthDeflection1d.java
@@ -1,7 +1,7 @@
 /* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
    modeler, Finite element mesher, Plugin architecture.
  
-    Copyright (C) 2003,2004,2005,2006,2007, by EADS CRC
+    Copyright (C) 2003,2004,2005,2006, by EADS CRC
  
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
@@ -20,95 +20,209 @@
 
 package org.jcae.mesh.bora.algo;
 
+import org.jcae.mesh.bora.ds.BCADGraphCell;
+import org.jcae.mesh.bora.ds.BSubMesh;
 import org.jcae.mesh.bora.ds.BDiscretization;
-import org.jcae.mesh.mesher.algos1d.UniformLengthDeflection;
-import org.jcae.mesh.mesher.algos1d.UniformLength;
-import org.jcae.mesh.mesher.algos1d.Compat1D2D;
 import org.jcae.mesh.mesher.ds.SubMesh1D;
-import org.jcae.mesh.mesher.ds.MMesh1D;
-import java.util.Iterator;
+import org.jcae.mesh.mesher.ds.MEdge1D;
+import org.jcae.mesh.mesher.ds.MNode1D;
+import org.jcae.mesh.cad.*;
+import java.util.ArrayList;
 import org.apache.log4j.Logger;
 
-// This class should probably be renamed; AlgoMesh1d could be the new name
 /**
- * Computes a new discretization of the edges using the local definition
- * of target length and target deflection for each edge.
+ * Computes a new discretization so that all edges have a uniform length.
+ * On each edge, compute the number of subdivisions so that all segments
+ * have the same length, which must be less than the given criterion.
+ * The previous discretization nodes and edges are deleted, and replaced
+ * by newer ones.
  */
-public class UniformLengthDeflection1d
+public class UniformLengthDeflection1d implements AlgoInterface
 {
 	private static Logger logger=Logger.getLogger(UniformLengthDeflection1d.class);
-	private MMesh1D mesh1d;
-	private UniformLengthDeflection algoUniformLengthDeflection;
-	private UniformLength           algoUniformLength;
-	private Compat1D2D              algoCompat1D2D;
-	
+	private double maxlen;
+	private double deflection;
+	private boolean relDefl;
+
 	/**
-	 * Creates a <code>UniformLengthDeflection1d</code> instance.
+	 * Discretizes a topological edge so that all edges have a uniform length.
+	 * For a given topological edge, its previous discretization is first
+	 * removed.  Then the number of segments is computed such that segment
+	 * length is inferior to the desired length.  The geometrical edge is then
+	 * divided into segments of uniform lengths.
 	 *
-	 * @param m  the <code>MMesh1D</code> instance to mesh.
+	 * @param len  target length
+	 * @param defl  target deflection
+	 * @param rel  <code>true</code> if deflection is relative,
+	 * <code>false</code> otherwise.
 	 */
-	public UniformLengthDeflection1d(MMesh1D m)
+	public UniformLengthDeflection1d(double len, double defl, boolean rel)
 	{
-		mesh1d = m;
-		algoUniformLengthDeflection = new UniformLengthDeflection(mesh1d);
-		algoUniformLength           = new UniformLength(mesh1d);
-		algoCompat1D2D              = new Compat1D2D(mesh1d);
+		maxlen = len;
+		deflection = defl;
+		relDefl = rel;
+	}
+
+	public boolean isAvailable()
+	{
+		return true;
+	}
+
+	public int getOrientation(int o)
+	{
+		return o;
 	}
 
 	/**
-	 * Explores each edge discretization of the mesh and calls the 
-	 * discretisation method.
+	 * @return <code>true</code> if this edge was successfully discrtetized,
+	 * <code>false</code> otherwise.
 	 */
-	public void compute(boolean relDefl)
+	public boolean compute(BDiscretization d)
 	{
-		int nbTEdges = 0, nbNodes = 0, nbEdges = 0;
-		double currentDiscrLength;
-		double currentDiscrDeflec;
-		/* Explore the shape for each edge */
-		Iterator ite = mesh1d.getTEdgeIterator();
-		/*  First compute current nbNodes and nbEdges  */
-		while (ite.hasNext())
+		int nbPoints;
+		boolean isCircular = false;
+		boolean isDegenerated = false;
+		double[] paramOnEdge;
+		double range[];
+		BCADGraphCell cell = d.getGraphCell();
+		CADEdge E = (CADEdge) cell.getShape();
+		SubMesh1D submesh1d = new SubMesh1D(E);
+		d.setMesh(submesh1d);
+		logger.debug(""+this+"  shape: "+E);
+		
+		ArrayList edgelist = submesh1d.getEdges();
+		ArrayList nodelist = submesh1d.getNodes();
+		if (edgelist.size() != 0 || nodelist.size() != 0)
+			return false;
+		edgelist.clear();
+		nodelist.clear();
+		CADVertex[] V = E.vertices();
+		if (V[0].isSame(V[1]))
+			isCircular=true;
+		
+		CADGeomCurve3D curve = CADShapeBuilder.factory.newCurve3D(E);
+		if (curve == null)
 		{
-		        BDiscretization discrE = (BDiscretization) ite.next();
-			SubMesh1D submesh1d = mesh1d.getSubMesh1DFromMap(discrE);
-			nbNodes += submesh1d.getNodes().size();
-			nbEdges += submesh1d.getEdges().size();
+			if (!E.isDegenerated())
+				throw new java.lang.RuntimeException("Curve not defined on edge, but this  edhe is not degenrerated.  Something must be wrong.");
+			
+			isDegenerated = true;
+			// Degenerated edges should not be discretized, but then
+			// their vertices have very low connectivity.  So let
+			// discretize them until a solution is found.
+			range = E.range();
+			nbPoints=2;
+			paramOnEdge = new double[nbPoints];
+			for (int i = 0; i < nbPoints; i++)
+				paramOnEdge[i] = range[0] + (range[1] - range[0])*i/(nbPoints-1);
 		}
-		ite = mesh1d.getTEdgeIterator();
-		while (ite.hasNext())
+		else
 		{
-		        BDiscretization discrE = (BDiscretization) ite.next();
-			SubMesh1D submesh1d = mesh1d.getSubMesh1DFromMap(discrE);
-			nbNodes -= submesh1d.getNodes().size();
-			nbEdges -= submesh1d.getEdges().size();
-
-			currentDiscrLength = discrE.getConstraint().getHypothesis().getLength();
-			currentDiscrDeflec = discrE.getConstraint().getHypothesis().getDeflection();
-
-			if (currentDiscrDeflec <= 0.0)
+			range = curve.getRange();
+			if (deflection > 0.0)
+				curve.discretize(maxlen, deflection, relDefl);
+			else
+				curve.discretize(maxlen);
+			nbPoints = curve.nbPoints();
+			int saveNbPoints =  nbPoints;
+			if (nbPoints <= 2 && !isCircular)
 			{
-				if (algoUniformLength.computeEdge(currentDiscrLength, submesh1d))
-					nbTEdges++;
+				//  Compute the deflection
+				double mid1[], pnt1[], pnt2[];
+
+				mid1 = curve.value((range[0] + range[1])/2.0);
+				pnt1 = V[0].pnt();
+				pnt2 = V[1].pnt();
+				double d1 =
+					(mid1[0] - 0.5*(pnt1[0]+pnt2[0])) * (mid1[0] - 0.5*(pnt1[0]+pnt2[0])) +
+					(mid1[1] - 0.5*(pnt1[1]+pnt2[1])) * (mid1[1] - 0.5*(pnt1[1]+pnt2[1])) +
+					(mid1[2] - 0.5*(pnt1[2]+pnt2[2])) * (mid1[2] - 0.5*(pnt1[2]+pnt2[2]));
+				double d2 =
+					(pnt1[0] - pnt2[0]) * (pnt1[0] - pnt2[0]) +
+					(pnt1[1] - pnt2[1]) * (pnt1[1] - pnt2[1]) +
+					(pnt1[2] - pnt2[2]) * (pnt1[2] - pnt2[2]);
+				if (d1 > 0.01 * d2 && (deflection <= 0 || d1 > 1.e-6 * maxlen * maxlen)) {
+					nbPoints=3;
+				} else {
+					nbPoints=2;
+				}
+			}
+			else if (nbPoints <= 3 && isCircular)
+				nbPoints=4;
+			if (saveNbPoints != nbPoints)
+				curve.discretize(nbPoints);
+			paramOnEdge = new double[nbPoints];
+			// GCPnts_UniformAbscissa is not very accurate, force paramOnEdge
+			// to be in ascending order.
+			int offset = 0;
+			paramOnEdge[0] = curve.parameter(1);
+			if (range[0] < range[1])
+			{
+				for (int i = 1; i < nbPoints; i++)
+				{
+					paramOnEdge[i-offset] = curve.parameter(i+1);
+					if (paramOnEdge[i-offset] <= paramOnEdge[i-offset-1])
+						offset++;
+				}
 			}
 			else
 			{
-				if (algoUniformLengthDeflection.computeEdge(currentDiscrLength, currentDiscrDeflec, relDefl, submesh1d))
+				for (int i = 1; i < nbPoints; i++)
 				{
-					nbTEdges++;
-					// Feature not available yet!
- 					// if (isotropic)
- 					// 	algoCompat1D2D.computeEdge(submesh1d, Set faceset, currentDiscrDeflec, relDefl);
-					// The computation of faceset still has to be done.
+					paramOnEdge[i-offset] = curve.parameter(i+1);
+					if (paramOnEdge[i-offset] >= paramOnEdge[i-offset-1])
+						offset++;
 				}
 			}
-			nbNodes += submesh1d.getNodes().size();
-			nbEdges += submesh1d.getEdges().size();
-			System.out.println(discrE+": nbNodes = "+submesh1d.getNodes().size()+" nbEdges = "+submesh1d.getEdges().size());
+			nbPoints -= offset;
 		}
-		logger.debug("TopoEdges discretisees "+nbTEdges);
-		logger.debug("Edges   "+nbEdges);
-		logger.debug("Nodes   "+nbNodes);
-		assert(mesh1d.isValid());
-	}
 
+		MNode1D n1, n2;
+		double param;
+
+		//  First vertex
+		BDiscretization dc = cell.getGraph().getByShape(V[0]).getDiscretizationSubMesh(d.getFirstSubMesh());
+		CADVertex GPt = (CADVertex) dc.getMesh();
+		MNode1D firstNode = new MNode1D(paramOnEdge[0], GPt);
+		n1 = firstNode;
+		n1.isDegenerated(isDegenerated);
+		nodelist.add(n1);
+		if (!isDegenerated)
+			GPt = null;
+
+		//  Other points
+		for (int i = 0; i < nbPoints - 1; i++)
+		{
+			param = paramOnEdge[i+1];
+			if (i == nbPoints - 2)
+			{
+				dc = cell.getGraph().getByShape(V[1]).getDiscretizationSubMesh(d.getFirstSubMesh());
+				GPt = (CADVertex) dc.getMesh();
+			}
+			n2 = new MNode1D(param, GPt);
+			n2.isDegenerated(isDegenerated);
+			nodelist.add(n2);
+			MEdge1D e=new MEdge1D(n1, n2, false);
+			edgelist.add(e);
+			n1 = n2;
+		}
+		if (logger.isDebugEnabled())
+		{
+			logger.debug("  Edges   "+edgelist.size());
+			logger.debug("  Nodes   "+nodelist.size());
+		}
+		return true;
+	}
+	
+	public String toString()
+	{
+		String ret = "Algo: "+getClass().getName();
+		ret += "\nTarget size: "+maxlen;
+		ret += "\nDeflection: "+deflection;
+		if (relDefl)
+			ret += " (relative)";
+		else
+			ret += " (absolute)";
+		return ret;
+	}
 }
