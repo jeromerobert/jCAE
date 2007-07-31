7514a346e0f0385c546a82f6d385a9f7dc19cc91
jeromerobert <jeromerobert>
2007-07-31 13:15:14 +0000

    Transpose modification of make-dist-Linux.sh to make-dist-Win32.sh
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1397 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- netbeans-suite/make-dist-Win32.sh
+++ netbeans-suite/make-dist-Win32.sh
@@ -1,5 +1,9 @@
-#! /bin/sh
+#! /bin/sh -xe
 
+# Change this to your own OpenCASCADE installation
+CASROOT=/home/jerome/OpenCASCADE6.2.0-win32/ros
+
+# Name of the Java directory in the final bundle
 JAVA_NAME=jre-6
 VERSION=jcae-@JCAE_VERSION@
 rm -rf $VERSION
@@ -16,11 +20,25 @@ set PATH=%jcaeHome%OpenCASCADE6.2.0/ros/win32/bin;%jcaeHome%jcae/modules/lib/;%P
 "%jcaeHome%/bin/jcae" --jdkhome "%jcaeHome%/$JAVA_NAME" -J-Xmx%MEMORY% -J-Djavax.media.j3d.zFactorAbs=%ZFACTORABS% -J-Djavax.media.j3d.zFactorRel=%ZFACTORREL%
 EOF
 
-ln -s ../jre-6-win32/ $JAVA_NAME
-rm -rf $JAVA_NAME/plugin $JAVA_NAME/javaw $JAVA_NAME/lib/i386/client/classes.jsa
+test -z "$JRE_HOME" && JRE_HOME=../jre-6-win32
+CASCADE_LIB=$CASROOT/win32-jcae/bin
+test ! -d "$CASCADE_LIB" && CASCADE_LIB=$CASROOT/win32/bin
+
+if [ ! -d "$CASCADE_LIB" ]; then
+	echo "Invalid OpenCASCADE directory. Cannot find $CASCADE_LIB"
+	exit 0
+fi
+
+if [ ! -d "$JRE_HOME" ]; then
+	echo "Invalid JRE directory. Cannot find $JRE_HOME"
+	exit 0
+fi
+
+ln -s $JRE_HOME $JAVA_NAME
+rm -rf $JAVA_NAME/plugin $JAVA_NAME/javaw $JAVA_NAME/lib/i386/client/classes.jsa || true
 mkdir -p OpenCASCADE6.2.0/ros/win32
 ln -s $CASROOT/../LICENSE OpenCASCADE6.2.0/LICENSE
-ln -s $CASROOT/win32-jcae/bin/ OpenCASCADE6.2.0/ros/win32/bin
+ln -s $CASCADE_LIB OpenCASCADE6.2.0/ros/win32/bin
 unzip ../dist/jcae.zip
 mv jcae tmp
 mv tmp/* .
@@ -28,6 +46,6 @@ rmdir tmp
 
 cd ..
 
-rm $VERSION-win32.zip
+rm $VERSION-win32.zip || true
 zip -9r "$VERSION-win32.zip" "$VERSION"
 
