2ef0ac3aa16955d510c052606025b66cd92efb32
barbier <barbier>
2007-09-02 19:32:34 +0000

    Cosmetics
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1465 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/amibe/ds/Vertex.java
+++ amibe/src/org/jcae/mesh/amibe/ds/Vertex.java
@@ -692,7 +692,7 @@ public class Vertex extends AbstractVertex implements Serializable
 	 * methods.
 	 * Note2: According to Pascal J. Frey, the key point is to have
 	 * reliable input.  We can have good approximation of quadrics
-	 * is the normal to the surface is accurate, and normal to the
+	 * if the normal to the surface is accurate, and normal to the
 	 * surface can be approximated accurately if the quadric is
 	 * precise.  So we should certainly read normals from a file
 	 * if they are available.
