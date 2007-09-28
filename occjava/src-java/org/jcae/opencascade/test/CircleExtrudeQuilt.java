0d3fd428e32e865e71e301afde7bd5441975df6a
jeromerobert <jeromerobert>
2007-09-28 08:33:15 +0000

    Write the output file to the system dependent temporary directory.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1633 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- occjava/src-java/org/jcae/opencascade/test/CircleExtrudeQuilt.java
+++ occjava/src-java/org/jcae/opencascade/test/CircleExtrudeQuilt.java
@@ -2,6 +2,7 @@ package org.jcae.opencascade.test;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.util.ArrayList;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -102,61 +103,74 @@ public class CircleExtrudeQuilt
 	
 	public static void main(String[] args)
 	{
-		//Create a holed cylinder
-		TopoDS_Edge circleB=createCircle(0, 0, -1E-3, 0, 0, 1, 4E-3);
-		TopoDS_Edge circleSP1=createCircle(0, 0, 0, 0, 0, 1, 1E-3);
-		TopoDS_Face cylinderS=extrude(circleSP1, 0, 0, -0.016);		
-		TopoDS_Face cylinderBzM=(TopoDS_Face) extrude(circleB, 0, 0, -0.015).reversed();		
-		TopoDS_Face cylinderBzP=extrude(circleB, 0, 0, 1E-3);
-		
-		TopoDS_Face hdSurface=(TopoDS_Face) createFace(
-			createWire(getLastEdge(cylinderBzM)),
-			createWire(getLastEdge(cylinderS))).reversed();		
-		
-		//Create a cylinder with a large disc sewed to it's base
-		
-		TopoDS_Face cylinderDiscP2=createFace(
-			createWire(getLastEdge(cylinderBzP)), createWire(circleSP1));
-		TopoDS_Face cylinderSzP=extrude(circleSP1, 0, 0, 7.5E-3);
-		TopoDS_Face cylinderDiscSzP=createFace(getLastEdge(cylinderSzP));
-		
-		//Create a box
-		TopoDS_Solid box=(TopoDS_Solid) new BRepPrimAPI_MakeBox(new double[]{4.5E-2,0.03,-0.001}, new double[]{-4.5E-2,-0.03,0}).shape();
-		//Make a hole in the box
-		TopoDS_Face[] boxFaces=getFaces(box);
-		TopoDS_Face squareZm=boxFaces[4];
-		TopoDS_Face squareZp=boxFaces[5];
-		squareZm=createFace(getWires(squareZm)[0], (TopoDS_Wire) createWire(circleB).reversed());
-		squareZp=createFace(getWires(squareZp)[0], getWires(cylinderDiscP2)[0]);
-		TopoDS_Face[] holedBoxFaces=new TopoDS_Face[]{
-			boxFaces[0],
-			boxFaces[1],
-			boxFaces[2],
-			boxFaces[3],
-			squareZm,
-			squareZp,
-			cylinderBzP};		
-		TopoDS_Solid holedBox=(TopoDS_Solid) new BRepBuilderAPI_MakeSolid(createShell(holedBoxFaces)[0]).shape();
-		
-		//Put it all together
-		BRep_Builder bb=new BRep_Builder();
-		TopoDS_Compound compound=new TopoDS_Compound();
-		bb.makeCompound(compound);
-		bb.add(compound, holedBox);
-		bb.add(compound, cylinderBzM);
-		bb.add(compound, cylinderS);
-		bb.add(compound, hdSurface);
-		bb.add(compound, cylinderDiscP2);
-		bb.add(compound, cylinderSzP);
-		bb.add(compound, cylinderDiscSzP);
-		bb.add(compound, cylinderBzP);		
-		
-		//save it
-		String file="/tmp/toto.brep";
-		BRepTools.write(compound, file);
-
-		//display(file);
-		//display("/tmp/Solid_1.brep");
+		try
+		{
+			//Create a holed cylinder
+			TopoDS_Edge circleB=createCircle(0, 0, -1E-3, 0, 0, 1, 4E-3);
+			TopoDS_Edge circleSP1=createCircle(0, 0, 0, 0, 0, 1, 1E-3);
+			TopoDS_Face cylinderS=extrude(circleSP1, 0, 0, -0.016);		
+			TopoDS_Face cylinderBzM=(TopoDS_Face) extrude(circleB, 0, 0, -0.015).reversed();		
+			TopoDS_Face cylinderBzP=extrude(circleB, 0, 0, 1E-3);
+			
+			TopoDS_Face hdSurface=(TopoDS_Face) createFace(
+				createWire(getLastEdge(cylinderBzM)),
+				createWire(getLastEdge(cylinderS))).reversed();		
+			
+			//Create a cylinder with a large disc sewed to it's base
+			
+			TopoDS_Face cylinderDiscP2=createFace(
+				createWire(getLastEdge(cylinderBzP)), createWire(circleSP1));
+			TopoDS_Face cylinderSzP=extrude(circleSP1, 0, 0, 7.5E-3);
+			TopoDS_Face cylinderDiscSzP=createFace(getLastEdge(cylinderSzP));
+			
+			//Create a box
+			TopoDS_Solid box=(TopoDS_Solid) new BRepPrimAPI_MakeBox(
+				new double[]{4.5E-2,0.03,-0.001},
+				new double[]{-4.5E-2,-0.03,0}).shape();
+			//Make a hole in the box
+			TopoDS_Face[] boxFaces=getFaces(box);
+			TopoDS_Face squareZm=boxFaces[4];
+			TopoDS_Face squareZp=boxFaces[5];
+			
+			squareZm=createFace(
+				getWires(squareZm)[0],
+				(TopoDS_Wire) createWire(circleB).reversed());
+			
+			squareZp=createFace(getWires(squareZp)[0], getWires(cylinderDiscP2)[0]);
+			TopoDS_Face[] holedBoxFaces=new TopoDS_Face[]{
+				boxFaces[0],
+				boxFaces[1],
+				boxFaces[2],
+				boxFaces[3],
+				squareZm,
+				squareZp,
+				cylinderBzP};		
+			TopoDS_Solid holedBox=(TopoDS_Solid) new BRepBuilderAPI_MakeSolid(
+				createShell(holedBoxFaces)[0]).shape();
+			
+			//Put it all together
+			BRep_Builder bb=new BRep_Builder();
+			TopoDS_Compound compound=new TopoDS_Compound();
+			bb.makeCompound(compound);
+			bb.add(compound, holedBox);
+			bb.add(compound, cylinderBzM);
+			bb.add(compound, cylinderS);
+			bb.add(compound, hdSurface);
+			bb.add(compound, cylinderDiscP2);
+			bb.add(compound, cylinderSzP);
+			bb.add(compound, cylinderDiscSzP);
+			bb.add(compound, cylinderBzP);		
+			
+			//save it		
+			BRepTools.write(compound, File.createTempFile("circleExtrude", ".brep").getPath());
+	
+			//display(file);
+			//display("/tmp/Solid_1.brep");
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
 	}
 	
 	/**
