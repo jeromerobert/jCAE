jeromerobert/jCAE

commit a20cf610de469462dcf4c4014e394ac35f29af58

* src/org/jcae/mesh/amibe/patch/Mesh2D.java   src/org/jcae/mesh/amibe/traits/MeshTraitsBuilder.java: Replace quadtree member from Mesh2D by a builder into MeshTraitsBuilder.
git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1642 9c4724a3-da1e-0410-bc68-88f18da35cd4

amibe/src/org/jcae/mesh/amibe/traits/MeshTraitsBuilder.java
@@ -21,6 +21,7 @@ package org.jcae.mesh.amibe.traits;
 
 import org.jcae.mesh.amibe.ds.AbstractTriangle;
 import org.jcae.mesh.amibe.ds.AbstractVertex;
+import org.jcae.mesh.amibe.util.KdTree;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Collection;
@@ -31,16 +32,20 @@ public class MeshTraitsBuilder extends TraitsBuilder
 	private static final int BITTRIANGLES = 8;
 	private static final int BITNODES     = 9;
 	private static final int BITGROUPS    = 10;
+	private static final int BITKDTREE    = 11;
 
 	private static final int TRIANGLES        = 1 << BITTRIANGLES;
 	private static final int NODES            = 1 << BITNODES;
 	private static final int GROUPLIST        = 1 << BITGROUPS;
-	private static final int TRIANGLESET      = 1 << (BITTRIANGLES+3);
-	private static final int NODESET          = 1 << (BITNODES+3);
+	private static final int KDTREE           = 1 << BITKDTREE;
+	private static final int TRIANGLESET      = 30;
+	private static final int NODESET          = 31;
 
 	private VertexTraitsBuilder vertexTraitsBuilder = new VertexTraitsBuilder();
 	private HalfEdgeTraitsBuilder halfedgeTraitsBuilder = new HalfEdgeTraitsBuilder();
 	private TriangleTraitsBuilder triangleTraitsBuilder = new TriangleTraitsBuilder();
+
+	private int dimension;
 
 	public MeshTraitsBuilder()
 	{
@@ -101,6 +106,11 @@ public class MeshTraitsBuilder extends TraitsBuilder
 		return hasCapability(TRIANGLES);
 	}
 
+	public boolean hasKdTree()
+	{
+		return hasCapability(KDTREE);
+	}
+
 	public MeshTraitsBuilder addGroupList()
 	{
 		attributes |= GROUPLIST;
@@ -111,6 +121,20 @@ public class MeshTraitsBuilder extends TraitsBuilder
 	{
 		if ((attributes & GROUPLIST) != 0)
 			return (Collection) t.array[index[BITGROUPS]];
+		return null;
+	}
+
+	public MeshTraitsBuilder addKdTree(int d)
+	{
+		attributes |= KDTREE;
+		dimension = d;
+		return this;
+	}
+
+	public KdTree getKdTree(Traits t)
+	{
+		if ((attributes & KDTREE) != 0)
+			return (KdTree) t.array[index[BITKDTREE]];
 		return null;
 	}
 
@@ -127,6 +151,8 @@ public class MeshTraitsBuilder extends TraitsBuilder
 			t.array[index[BITNODES]] = new ArrayList<AbstractVertex>();
 		if ((attributes & GROUPLIST) != 0)
 			t.array[index[BITGROUPS]] = new ArrayList();
+		if ((attributes & KDTREE) != 0)
+			t.array[index[BITKDTREE]] = new KdTree(dimension);
 	}
 
 	public MeshTraitsBuilder add(TraitsBuilder t)

amibe/src/org/jcae/mesh/amibe/algos2d/Initial.java
@@ -231,7 +231,7 @@ public class Initial
 		}
 		if (bbmax[0] <= bbmin[0] || bbmax[1] <= bbmin[1])
 			throw new InvalidFaceException();
-		mesh.initQuadTree(bbmin, bbmax);
+		mesh.resetQuadTree(bbmin, bbmax);
 		//  Initial point insertion sometimes fail on 2D,
 		//  this needs to be investigated.
 		mesh.pushCompGeom(2);

amibe/src/org/jcae/mesh/amibe/patch/Mesh2D.java
@@ -57,11 +57,6 @@ public class Mesh2D extends Mesh
 	//  Stack of methods to compute geometrical values
 	private transient final Stack<Calculus> compGeomStack = new Stack<Calculus>();
 	
-	/**
-	 * Structure to fasten search of nearest vertices.
-	 */
-	private transient KdTree quadtree = null;
-	
 	// Utility class to improve debugging output
 	private static class OuterVertex2D extends Vertex2D
 	{
@@ -85,9 +80,9 @@ public class Mesh2D extends Mesh
 	 */
 	public Mesh2D()
 	{
-		super();
-		MeshTraitsBuilder mtb = new MeshTraitsBuilder();
-		factory = new ElementPatchFactory(mtb);
+		// Always build a quadtree
+		super(new MeshTraitsBuilder().addKdTree(2));
+		factory = new ElementPatchFactory(traitsBuilder);
 		face = null;
 		surface = null;
 		init();
@@ -95,8 +90,9 @@ public class Mesh2D extends Mesh
 
 	public Mesh2D(MeshTraitsBuilder mtb)
 	{
-		super(mtb);
-		factory = new ElementPatchFactory(mtb);
+		// Always build a quadtree
+		super(mtb.addKdTree(2));
+		factory = new ElementPatchFactory(traitsBuilder);
 		face = null;
 		surface = null;
 		init();
@@ -115,8 +111,8 @@ public class Mesh2D extends Mesh
 	 */
 	public Mesh2D(MeshTraitsBuilder mtb, CADShape f)
 	{
-		super(mtb);
-		factory = new ElementPatchFactory(mtb);
+		super(mtb.addKdTree(2));
+		factory = new ElementPatchFactory(traitsBuilder);
 		face = f;
 		surface = ((CADFace) face).getGeomSurface();
 		init();
@@ -124,9 +120,9 @@ public class Mesh2D extends Mesh
 
 	public Mesh2D(CADShape f)
 	{
-		super();
-		MeshTraitsBuilder mtb = new MeshTraitsBuilder();
-		factory = new ElementPatchFactory(mtb);
+		// Always build a quadtree
+		super(new MeshTraitsBuilder().addKdTree(2));
+		factory = new ElementPatchFactory(traitsBuilder);
 		face = f;
 		surface = ((CADFace) face).getGeomSurface();
 		init();
@@ -193,11 +189,23 @@ public class Mesh2D extends Mesh
 	}
 	
 	/**
-	 * Initialize a KdTree with a given bounding box.
+	 * Returns the quadtree associated with this mesh.
+	 *
+	 * @return the quadtree associated with this mesh.
+	 */
+	public KdTree getQuadTree()
+	{
+		return traitsBuilder.getKdTree(traits);
+	}
+	
+	/**
+	 * Initializes quadtree with a given bounding box.  This method must be called before putting
+	 * any vertex into this quadtree.
+	 *
 	 * @param bbmin  coordinates of bottom-left vertex
 	 * @param bbmax  coordinates of top-right vertex
 	 */
-	public void initQuadTree(double [] bbmin, double [] bbmax)
+	public void resetQuadTree(double [] bbmin, double [] bbmax)
 	{
 		double [] bbox = new double[2*bbmin.length];
 		for (int i = 0; i < bbmin.length; i++)
@@ -205,18 +213,9 @@ public class Mesh2D extends Mesh
 			bbox[i] = bbmin[i];
 			bbox[i+bbmin.length] = bbmax[i];
 		}
-		quadtree = new KdTree(bbox);
+		KdTree quadtree = traitsBuilder.getKdTree(traits);
+		quadtree.setup(bbox);
 		outerVertex = new OuterVertex2D((bbmin[0]+bbmax[0])*0.5, (bbmin[1]+bbmax[1])*0.5);
-	}
-	
-	/**
-	 * Returns the quadtree associated with this mesh.
-	 *
-	 * @return the quadtree associated with this mesh.
-	 */
-	public KdTree getQuadTree()
-	{
-		return quadtree;
 	}
 	
 	/**
@@ -228,6 +227,7 @@ public class Mesh2D extends Mesh
 	@Override
 	public Collection<AbstractVertex> getNodes()
 	{
+		KdTree quadtree = traitsBuilder.getKdTree(traits);
 		if (quadtree == null)
 			return null;
 		return quadtree.getAllVertices(triangleList.size() / 2);
@@ -243,6 +243,7 @@ public class Mesh2D extends Mesh
 	 */
 	public void bootstrap(Vertex2D v0, Vertex2D v1, Vertex2D v2)
 	{
+		KdTree quadtree = traitsBuilder.getKdTree(traits);
 		assert quadtree != null;
 		assert v0.onLeft(this, v1, v2) != 0L;
 		if (v0.onLeft(this, v1, v2) < 0L)
@@ -467,6 +468,7 @@ public class Mesh2D extends Mesh
 	 */
 	private void clearAllMetrics()
 	{
+		KdTree quadtree = traitsBuilder.getKdTree(traits);
 		if (quadtree == null)
 			return;
 		ClearAllMetricsProcedure gproc = new ClearAllMetricsProcedure();

amibe/src/org/jcae/mesh/amibe/patch/tests/QuadTreeTestAdd.java
@@ -62,7 +62,7 @@ public class QuadTreeTestAdd extends QuadTreeTest
 		double [] bbmax = { 1.0, 1.0 };
 		final Mesh2D m = new Mesh2D();
 		m.pushCompGeom(2);
-		m.initQuadTree(bbmin, bbmax);
+		m.resetQuadTree(bbmin, bbmax);
 		final QuadTreeTestAdd r = new QuadTreeTestAdd(m.getQuadTree());
 		logger.debug("Start insertion");
 		for (int i = 0; i < 500; i++)

amibe/src/org/jcae/mesh/amibe/patch/tests/QuadTreeTestNearest.java
@@ -68,7 +68,7 @@ public class QuadTreeTestNearest extends QuadTreeTest
 		double [] bbmax = { 1.0, 1.0 };
 		final Mesh2D m = new Mesh2D();
 		m.pushCompGeom(2);
-		m.initQuadTree(bbmin, bbmax);
+		m.resetQuadTree(bbmin, bbmax);
 		final QuadTreeTestNearest r = new QuadTreeTestNearest(m.getQuadTree());
 		logger.debug("Start insertion");
 		for (int i = 0; i < 500; i++)

amibe/src/org/jcae/mesh/amibe/patch/tests/QuadTreeTestRemove.java
@@ -72,7 +72,7 @@ public class QuadTreeTestRemove extends QuadTreeTest
 		double [] bbmax = { 1.0, 1.0 };
 		final Mesh2D m = new Mesh2D();
 		m.pushCompGeom(2);
-		m.initQuadTree(bbmin, bbmax);
+		m.resetQuadTree(bbmin, bbmax);
 		final QuadTreeTestRemove r = new QuadTreeTestRemove(m.getQuadTree());
 		logger.debug("Start insertion");
 		for (int i = 0; i < 500; i++)

amibe/src/org/jcae/mesh/amibe/util/KdTree.java
@@ -226,6 +226,19 @@ public class KdTree
 	public final double [] x0;
 	
 	/**
+	 * Dummy constructor.  This instance must be properly initialised by calling
+	 * {@link #setup} before putting elements into it.
+	 */
+	public KdTree(int d)
+	{
+		BUCKETSIZE = 10;
+		dimension = d;
+		nrSub = 1 << dimension;
+		x0 = new double[dimension+1];
+		root = new Cell();
+	}
+	
+	/**
 	 * Create a new <code>KdTree</code> of the desired size.
 	 *
 	 * @param bbox   coordinates of bottom-left vertex and upper-right vertices
@@ -237,7 +250,6 @@ public class KdTree
 		nrSub = 1 << dimension;
 		x0 = new double[dimension+1];
 		root = new Cell();
-		nCells++;
 		setup(bbox);
 	}
 	
@@ -256,12 +268,16 @@ public class KdTree
 		nrSub = 1 << dimension;
 		x0 = new double[dimension+1];
 		root = new Cell();
-		nCells++;
 		setup(bbox);
 	}
 	
-	private final void setup(double [] bbox)
+	public final void setup(double [] bbox)
 	{
+		if (bbox.length != 2*dimension)
+			throw new IllegalArgumentException();
+		if (nCells > 0)
+			throw new RuntimeException("KdTree.setup() cannot be called after KdTree has been modified");
+		nCells = 1;
 		double maxDelta = 0.0;
 		for (int i = 0; i < dimension; i++)
 		{
@@ -345,6 +361,8 @@ public class KdTree
 	 */
 	public void add(Vertex v)
 	{
+		if (nCells == 0)
+			throw new RuntimeException("KdTree.setup() must be called before KdTree.add()");
 		Cell current = root;
 		int s = gridSize;
 		int [] ij = new int[dimension];
@@ -414,6 +432,8 @@ public class KdTree
 	 */
 	public void remove(Vertex v)
 	{
+		if (nCells == 0)
+			throw new RuntimeException("KdTree.setup() must be called before KdTree.remove()");
 		Cell current = root;
 		Cell last = root;
 		Cell next;

amibe/src/org/jcae/mesh/xmldata/MeshReader.java
@@ -135,7 +135,7 @@ public class MeshReader
 						bbmin[k] = coord[k];
 				}
 			}
-			mesh.initQuadTree(bbmin, bbmax);
+			mesh.resetQuadTree(bbmin, bbmax);
 			for (int i=0; i < numberOfNodes; i++)
 				mesh.getQuadTree().add(nodelist[i]);
 			

commit 111dd99fb08d8338e39ceca37a0813655de49e5d

Fix a bug introduced in r1642
git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1692 9c4724a3-da1e-0410-bc68-88f18da35cd4

amibe/src/org/jcae/mesh/amibe/traits/MeshTraitsBuilder.java
@@ -39,8 +39,8 @@ public class MeshTraitsBuilder extends TraitsBuilder
 	private static final int NODES            = 1 << BITNODES;
 	private static final int GROUPLIST        = 1 << BITGROUPS;
 	private static final int KDTREE           = 1 << BITKDTREE;
-	private static final int TRIANGLESET      = 30;
-	private static final int NODESET          = 31;
+	private static final int TRIANGLESET      = 1 << 30;
+	private static final int NODESET          = 1 << 31;
 
 	private VertexTraitsBuilder vertexTraitsBuilder = new VertexTraitsBuilder();
 	private HalfEdgeTraitsBuilder halfedgeTraitsBuilder = new HalfEdgeTraitsBuilder();