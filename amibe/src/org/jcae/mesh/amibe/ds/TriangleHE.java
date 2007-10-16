a9d7ed58bc98540bb9f019b69180808b5c549ec3
barbier <barbier>
2007-10-16 10:33:43 +0000

    * src/org/jcae/mesh/amibe/ds/AdjacencyVH.java   src/org/jcae/mesh/amibe/ds/TriangleVH.java   src/org/jcae/mesh/amibe/ds/AdjacencyHE.java   src/org/jcae/mesh/amibe/ds/TriangleHE.java: Make AdjacencyVH an inner class of TriangleVH and AdjacencyHE an inner class of TriangleHE.
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1796 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src/org/jcae/mesh/amibe/ds/TriangleHE.java
+++ amibe/src/org/jcae/mesh/amibe/ds/TriangleHE.java
@@ -46,4 +46,222 @@ public class TriangleHE extends Triangle
 		((AdjacencyHE) adj).setHalfEdge(0, e);
 	}
 
+	private static class AdjacencyHE implements AdjacencyWrapper
+	{
+		/**
+		 * Pointers to adjacent elements through edges.
+		 */
+		private HalfEdge edge = null;
+	
+		public final void copy(AdjacencyWrapper that)
+		{
+			if (!(that instanceof AdjacencyHE))
+				throw new IllegalArgumentException();
+			AdjacencyHE src = (AdjacencyHE) that;
+			HalfEdge to = edge;
+			HalfEdge from = src.edge;
+			for (int i = 0; i < 3; i++)
+			{
+				to.copy(from);
+				to = (HalfEdge) to.next();
+				from = (HalfEdge) from.next();
+			}
+		}
+		
+		public HalfEdge getHalfEdge(int num)
+		{
+			if (num == 0)
+				return edge;
+			else if (num == 1)
+				return (HalfEdge) edge.next();
+			else if (num == 2)
+				return (HalfEdge) edge.prev();
+			else
+				throw new RuntimeException();
+		}
+	
+		public void setHalfEdge(int num, HalfEdge e)
+		{
+			if (num == 0)
+				edge = e;
+			else if (num == 1)
+				edge.setNext(e);
+			else if (num == 2)
+				((HalfEdge) edge.next()).setNext(e);
+			else
+				throw new RuntimeException();
+		}
+	
+		public Object getAdj(int num)
+		{
+			return getHalfEdge(num).getAdj();
+		}
+	
+		public void setAdj(int num, Object link)
+		{
+			getHalfEdge(num).setAdj(link);
+		}
+	
+		public int getAdjLocalNumber(int num)
+		{
+			return getHalfEdge(num).getLocalNumber();
+		}
+	
+		public void setAdjLocalNumber(int num, int pos)
+		{
+			throw new RuntimeException();
+		}
+	
+		// Helper functions
+		public boolean hasFlag(int flag)
+		{
+			return (edge.hasAttributes(flag) | edge.next().hasAttributes(flag) | edge.prev().hasAttributes(flag));
+		}
+	
+		public void setFlag(int flag)
+		{
+			edge.setAttributes(flag);
+			edge.next().setAttributes(flag);
+			edge.prev().setAttributes(flag);
+		}
+	
+		public void clearFlag(int flag)
+		{
+			edge.clearAttributes(flag);
+			edge.next().clearAttributes(flag);
+			edge.prev().clearAttributes(flag);
+		}
+	
+		/**
+		 * Return the {@link AbstractHalfEdge#OUTER} attribute of its edges.
+		 *
+		 * @return <code>true</code> if the triangle is outer,
+		 * <code>false</code> otherwise.
+		 */
+		public boolean isOuter()
+		{
+			return hasFlag(AbstractHalfEdge.OUTER);
+		}
+		
+		/**
+		 * Set the {@link AbstractHalfEdge#OUTER} attribute of its three edges.
+		 */
+		public void setOuter()
+		{
+			setFlag(AbstractHalfEdge.OUTER);
+		}
+		
+		/**
+		 * Return the {@link AbstractHalfEdge#MARKED} attribute of its edges.
+		 *
+		 * @return <code>true</code> if an edge of this triangle has its
+		 * {@link AbstractHalfEdge#MARKED} attribute set, <code>false</code> otherwise.
+		 */
+		public boolean isMarked()
+		{
+			return hasFlag(AbstractHalfEdge.MARKED);
+		}
+		
+		/**
+		 * Set the {@link AbstractHalfEdge#MARKED} attribute of its three edges.
+		 */
+		public void setMarked()
+		{
+			setFlag(AbstractHalfEdge.MARKED);
+		}
+		
+		/**
+		 * Clear the {@link AbstractHalfEdge#MARKED} attribute of its three edges.
+		 */
+		public void unsetMarked()
+		{
+			edge.clearAttributes(AbstractHalfEdge.MARKED);
+			edge.next().clearAttributes(AbstractHalfEdge.MARKED);
+			edge.prev().clearAttributes(AbstractHalfEdge.MARKED);
+		}
+		
+		/**
+		 * Return the {@link AbstractHalfEdge#BOUNDARY} attribute of its edges.
+		 *
+		 * @return <code>true</code> if an edge of this triangle has its
+		 * {@link AbstractHalfEdge#BOUNDARY} attribute set, <code>false</code>
+		 * otherwise.
+		 */
+		public boolean isBoundary()
+		{
+			return hasFlag(AbstractHalfEdge.BOUNDARY);
+		}
+		
+		public boolean isReadable()
+		{
+			throw new RuntimeException("Not implemented yet");
+		}
+		
+		public boolean isWritable()
+		{
+			throw new RuntimeException("Not implemented yet");
+		}
+		
+		public void setReadable(boolean b)
+		{
+			throw new RuntimeException("Not implemented yet");
+		}
+		
+		public void setWritable(boolean b)
+		{
+			throw new RuntimeException("Not implemented yet");
+		}
+		
+		public int getEdgeAttributes(int num)
+		{
+			return getHalfEdge(num).getAttributes();
+		}
+		
+		public void setEdgeAttributes(int num, int attributes)
+		{
+			getHalfEdge(num).setAttributes((byte) attributes);
+		}
+		
+		private static String showHalfEdge(AbstractHalfEdge a)
+		{
+			HalfEdge e = (HalfEdge) a;
+			if (e == null)
+				return "null";
+			StringBuilder ret = new StringBuilder(""+e.hashCode()+"(");
+			if (e.getAdj() == null)
+				ret.append("null");
+			else
+				ret.append(e.getAdj().hashCode());
+			ret.append(")");
+			return ret.toString();
+		}
+	
+		private static String showSym(AbstractHalfEdge a)
+		{
+			HalfEdge e = (HalfEdge) a;
+			if (e == null)
+				return "null";
+			else if (e.getAdj() == null)
+				return "null";
+			else if (e.getAdj() instanceof HalfEdge)
+			{
+				HalfEdge sym = (HalfEdge) e.sym();
+				return ""+sym.getTri().hashCode()+"["+sym.getLocalNumber()+"]";
+			}
+			else
+				return "NM";
+		}
+	
+		@Override
+		public String toString()
+		{
+			StringBuilder r = new StringBuilder(50);
+			r.append("Halfedge: "+showHalfEdge(edge)+" "+showHalfEdge(edge.next())+" "+showHalfEdge(edge.prev()));
+			r.append("\nSym: "+showSym(edge)+" "+showSym(edge.next())+" "+showSym(edge.prev()));
+			r.append("\nEdge attributes:");
+			for (int i = 0; i < 3; i++)
+				r.append(" "+Integer.toHexString(getHalfEdge(i).getAttributes()));
+			return r.toString();
+		}
+	}
 }
