256c4558cf34eabd4d47661ab648b746b90df0a2
barbier <barbier>
2007-10-31 16:23:02 +0000

    Add command-line options
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1881 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/groovy/amibeReporter.groovy
+++ amibe/groovy/amibeReporter.groovy
@@ -9,19 +9,60 @@ import org.jcae.mesh.xmldata.MeshExporter;
 import org.jcae.mesh.amibe.validation.MinAngleFace;
 import org.jcae.mesh.amibe.validation.QualityFloat;
 import java.io.File;
+import gnu.trove.TIntHashSet;
+import org.apache.commons.cli.*;
 
-void usage() {
-	println("Usage: amibeReporter.groovy xmlDir outBase");
-	System.exit(1);
+void usage(int rc, Options options)
+{
+	HelpFormatter formatter = new HelpFormatter();
+	formatter.printHelp("groovy amibeReporter.groovy [options] xmlDir", options);
+	System.exit(rc);
 }
 
-if (args.length < 2)
-	usage();
-String xmlDir = args[0]
+Options options = new Options();
+options.addOption(
+	OptionBuilder.hasArg(false)
+		.withDescription("usage information")
+		.withLongOpt("help")
+		.create('h'));
+options.addOption(
+	OptionBuilder.withArgName("LIST").hasArg()
+		.withDescription("comma separated list of values (default: 0.2,0.4,0.6,0.8)")
+		.withLongOpt("bounds")
+		.create('b'));
+options.addOption(
+	OptionBuilder.hasArg(false)
+		.withDescription("reports statistics by face")
+		.withLongOpt("detailed")
+		.create('d'));
+options.addOption(
+	OptionBuilder.withArgName("LIST").hasArg()
+		.withDescription("comma separated list of faces (default: all faces)")
+		.withLongOpt("faces")
+		.create('f'));
+options.addOption(
+	OptionBuilder.withArgName("BASE").hasArg()
+		.withDescription("creates <BASE>.mesh and <BASE>.bb MEDIT files")
+		.withLongOpt("output")
+		.create('o'));
+CommandLineParser parser = new GnuParser();
+CommandLine cmd = parser.parse(options, args, true);
+if (cmd.hasOption('h'))
+	usage(0, options);
+
+String [] remaining = cmd.getArgs();
+if (remaining.length != 1)
+	usage(1, options);
+
+String xmlDir = remaining[0]
 String xmlFile = "jcae3d";
-String outBasename=args[1];
-String MESHName=outBasename+".mesh";
-String bbfile=outBasename+".bb";
+String outBasename=cmd.getOptionValue('o');
+boolean detailed = cmd.hasOption('d');
+
+String [] sBounds=cmd.getOptionValue('b', "0.2,0.4,0.6,0.8").split(",");
+Float [] bounds = new Float[sBounds.length];
+for (int i = 0; i < bounds.length; i++)
+	bounds[i] = Float.parseFloat(sBounds[i]);
 
 // Use an empty MeshTraitsBuilder to lower memory usage
 MeshTraitsBuilder mtb = new MeshTraitsBuilder();
@@ -37,20 +78,66 @@ catch (IOException ex)
 }
 // Compute mesh quality
 MinAngleFace qproc = new MinAngleFace();
-QualityFloat data = new QualityFloat(mesh3D.getTriangles().size());
-data.setQualityProcedure(qproc);
+int nrFaces = 1;
+if (detailed)
+{
+	TIntHashSet groups = new TIntHashSet(mesh3D.getTriangles().size());
+	for (AbstractTriangle f: mesh3D.getTriangles())
+	{
+		if (f.isWritable())
+		{
+			int i = f.getGroupId();
+			if (i >= 0)
+				groups.add(i);
+		}
+	}
+	nrFaces = groups.size();
+}
+QualityFloat [] data = new QualityFloat[nrFaces];
+int mean = mesh3D.getTriangles().size() / nrFaces;
+for (int i = 0; i < data.length; i++)
+{
+	data[i] = new QualityFloat(mean);
+	data[i].setQualityProcedure(qproc);
+}
 for (AbstractTriangle f: mesh3D.getTriangles())
 {
 	if (f.isWritable())
-		data.compute(f);
+	{
+		int i = f.getGroupId();
+		if (i < 0 || !detailed)
+			i = 0;
+		data[i].compute(f);
+	}
 }
-data.finish();
 float target = (float) Math.PI/3.0f;
-data.setTarget(target);
-data.split(0.4f, 0.6f, 0.8f, 0.9f, 1.0f);
-// Prints histogram on console
-data.printLayers();
-// Prints triangle quality into bbfile, it can be displayed by medit
-data.printMeshBB(bbfile);
-new MeshExporter.MESH(xmlDir).write(MESHName);
+for (int i = 0; i < data.length; i++)
+{
+	data[i].finish();
+	data[i].setTarget(target);
+	data[i].split(bounds);
+	// Prints histogram on console
+	if (detailed)
+		println("Face "+(i+1));
+	data[i].printLayers();
+}
+if (null != outBasename)
+{
+	// Prints triangle quality into a .bb file to be displayed by MEDIT
+	if (detailed)
+	{
+		int [] ids = new int[1];
+		for (int i = 0; i < data.length; i++)
+		{
+			data[i].printMeshBB(outBasename+"-"+i+".bb");
+			ids[0] = i;
+			new MeshExporter.MESH(new File(xmlDir), ids).write(outBasename+"-"+i+".mesh");
+		}
+	}
+	else
+	{
+		data[0].printMeshBB(outBasename+".bb");
+		new MeshExporter.MESH(xmlDir).write(outBasename+".mesh");
+	}
+}
 
--- amibe/groovy/unv2amibe.groovy
+++ amibe/groovy/unv2amibe.groovy
@@ -5,7 +5,37 @@ import org.jcae.mesh.amibe.ds.Mesh;
 import org.jcae.mesh.amibe.traits.MeshTraitsBuilder;
 import org.jcae.mesh.xmldata.MeshWriter;
 import org.jcae.mesh.amibe.util.UNVReader;
+import org.apache.commons.cli.*;
 
+void usage(int rc, Options options)
+{
+	HelpFormatter formatter = new HelpFormatter();
+	formatter.printHelp("groovy unv2amibe.groovy [options] unvFile", options);
+	System.exit(rc);
+}
+
+Options options = new Options();
+options.addOption(
+	OptionBuilder.hasArg(false)
+		.withDescription("usage information")
+		.withLongOpt("help")
+		.create('h'));
+options.addOption(
+	OptionBuilder.withArgName("PATH").hasArg()
+		.withDescription("output directory (default is current directory)")
+		.withLongOpt("outputdir")
+		.create('o'));
+CommandLineParser parser = new GnuParser();
+CommandLine cmd = parser.parse(options, args, true);
+if (cmd.hasOption('h'))
+	usage(0, options);
+
+String [] remaining = cmd.getArgs();
+if (remaining.length != 1)
+	usage(1, options);
+
+String unvFile = remaining[0];
+String outDir = cmd.getOptionValue('o', '.');
 Mesh mesh = new Mesh(new MeshTraitsBuilder());
-UNVReader.readMesh(mesh, args[0]);
-MeshWriter.writeObject3D(mesh, args[1], "jcae3d", ".", "dummy.brep");
+UNVReader.readMesh(mesh, unvFile);
+MeshWriter.writeObject3D(mesh, outDir, "jcae3d", ".", "dummy.brep");
