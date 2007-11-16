c5b0ff0ef16aecf20256ef519b6be7df830ea202
barbier <barbier>
2007-11-16 17:34:08 +0000

    Work in progress
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@1957 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/groovy/amibeSmoother.groovy
+++ amibe/groovy/amibeSmoother.groovy
@@ -4,10 +4,12 @@
 import org.jcae.mesh.amibe.ds.Mesh;
 import org.jcae.mesh.xmldata.MeshReader;
 import org.jcae.mesh.xmldata.MeshWriter;
-import org.jcae.mesh.amibe.algos3d.SmoothNodes3D;
+import org.jcae.mesh.amibe.algos3d.*;
 import java.io.File;
 import java.io.IOException;
 import org.apache.commons.cli.*;
+import org.jcae.mesh.amibe.validation.*
+import org.jcae.mesh.amibe.ds.Triangle;
 
 void usage(int rc, Options options)
 {
@@ -42,6 +44,11 @@ options.addOption(
 		.withDescription("do not try to preserve patch boundaries")
 		.withLongOpt("no-boundaries")
 		.create('n'));
+options.addOption(
+	OptionBuilder.hasArg(false)
+		.withDescription("update triangle quality within loop")
+		.withLongOpt("refresh")
+		.create('r'));
 CommandLineParser parser = new GnuParser();
 CommandLine cmd = parser.parse(options, args, true);
 if (cmd.hasOption('h'))
@@ -58,6 +65,7 @@ String xmlFile = "jcae3d";
 String sIter=cmd.getOptionValue('i', "5");
 String sSize=cmd.getOptionValue('s', "-1.0");
 String sTolerance=cmd.getOptionValue('t', "2.0");
+String sRefresh=String.valueOf(cmd.hasOption('r'));
 boolean bnd=!cmd.hasOption('n');
 
 Mesh mesh = new Mesh();
@@ -70,12 +78,37 @@ catch (IOException ex)
 	println("File "+xmlDir+File.separator+"jcae3d does not exist!");
 	usage();
 }
+HashMap<String, String> decOptions = new HashMap<String, String>();
+decOptions.put("size", "1");
+//new LengthDecimateHalfEdge(mesh, decOptions).compute();
+
 HashMap<String, String> opts = new HashMap<String, String>();
 opts.put("iterations", sIter);
 opts.put("boundaries", ""+bnd);
 opts.put("size", sSize);
 opts.put("tolerance", sTolerance);
-new SmoothNodes3D(mesh, opts).compute();
+opts.put("refresh", sRefresh);
+SmoothNodes3D sm = new SmoothNodes3D(mesh, opts)
+sm.setProgressBarStatus(10000);
+sm.compute();
 
 MeshWriter.writeObject3D(mesh, outDir, "jcae3d", ".", "dummy.brep");
 
+QualityFloat data = new QualityFloat(mesh.getTriangles().size());
+data.setQualityProcedure(new MaxLengthFace());
+for (Triangle f: mesh.getTriangles())
+{
+	if (f.isWritable())
+		data.compute(f);
+}
+data.finish();
+data.printStatistics();
+float lmin = data.getValueByPercent(0.0);
+float lmax = data.getValueByPercent(1.0);
+
+float c = 340.0f;
+float fmax = 1000.0f * c/lmax/6.0f
+println("Max frequency: "+fmax+"Hz ("+lmax+"mm)")
+float l2 = data.getValueByPercent(0.98);
+float f2 = 1000.0f * c/l2/10.0f
+println("Frequency for 2% criterion: "+f2+"Hz ("+l2+"mm)")
