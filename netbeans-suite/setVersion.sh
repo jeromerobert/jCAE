a36e0d09bfc55e97c1a33d4e30a1b810049e68bf
jeromerobert <jeromerobert>
2008-02-04 16:29:33 +0000

    Fix various bugs
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2203 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- netbeans-suite/setVersion.sh
+++ netbeans-suite/setVersion.sh
@@ -3,11 +3,11 @@ files="
 	branding/core/core.jar/org/netbeans/core/startup/Bundle.properties \
 	branding/modules/org-netbeans-core.jar/org/netbeans/core/ui/Bundle.properties \
 	branding/modules/org-netbeans-core-windows.jar/org/netbeans/core/windows/view/ui/Bundle.properties \
-	jcae-netbeans/manifest.mf \
+	core/manifest.mf \
 	make-dist-Linux.sh \
 	make-dist-Win32.sh \
 	nbproject/project.properties \
-	occjava/manifest.mf"
+	occjava-nb/manifest.mf"
 
 for file in $files; do
 	sed -i "s/@JCAE_VERSION@/$1/g" $file
