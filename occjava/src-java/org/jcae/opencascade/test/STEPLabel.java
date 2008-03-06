da0c707eab1c00b20af8431280a97bca969d831d
jeromerobert <jeromerobert>
2008-03-06 11:34:50 +0000

    Add an example to show how to dump the label from the edges of a STEP file
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2264 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ occjava/src-java/org/jcae/opencascade/test/STEPLabel.java
@@ -0,0 +1,50 @@
+/*
+ * Project Info:  http://jcae.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * (C) Copyright 2008, by EADS France
+ */
+
+package org.jcae.opencascade.test;
+
+import org.jcae.opencascade.jni.STEPControl_Reader;
+import org.jcae.opencascade.jni.TopAbs_ShapeEnum;
+import org.jcae.opencascade.jni.TopExp_Explorer;
+import org.jcae.opencascade.jni.TopoDS_Shape;
+
+/**
+ * Example which dump the label from the edges of a STEP file
+ * @author Jerome Robert
+ */
+public class STEPLabel
+{
+	public static void main(final String[] args)
+	{
+		STEPControl_Reader aReader = new STEPControl_Reader();
+		aReader.readFile("/tmp/Un_autre_essai.stp".getBytes());
+		aReader.nbRootsForTransfer();
+		aReader.transferRoots();
+		TopoDS_Shape s = aReader.oneShape();
+		TopExp_Explorer ex = new TopExp_Explorer(s, TopAbs_ShapeEnum.EDGE);
+		int i = 0;
+		while(ex.more())
+		{
+			i++;
+			System.out.println(i+ " " + aReader.getLabel(ex.current()));
+			ex.next();
+		}
+	}
+}
