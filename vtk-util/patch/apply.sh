559871e2b16e10842b38ea82a0e5b28347ac9411
barbier <barbier>
2008-07-09 22:37:43 +0000

    Patches can be reverted by modifying a single line of these scripts
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2368 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- vtk-util/patch/apply.sh
+++ vtk-util/patch/apply.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# Script that apply all the patchs in correct order to the VTK library. You must be on the root of the VTK source directory when executing this script.
+# Script that apply all the patchs in correct order to the VTK library.
+# You must be on the root of the VTK source directory when executing this script.
 
+patch_opts=  # set to -R to unapply patches
 scriptDir=$(dirname $0)
 echo "Applying depth patch"
-patch -p1 < $scriptDir/01_depth.patch
+patch -p1 $patch_opts < $scriptDir/01_depth.patch
 echo "Applying display list patch"
-patch -p0 < $scriptDir/02_displayList.patch
+patch -p0 $patch_opts < $scriptDir/02_displayList.patch
 echo "Applying offset patch"
-patch -p0 < $scriptDir/03_offset.patch
+patch -p0 $patch_opts < $scriptDir/03_offset.patch
 echo "Applying translucent patch"
-patch -p1 < $scriptDir/04_translucent.patch
+patch -p1 $patch_opts < $scriptDir/04_translucent.patch
 echo "Applying underscore patch"
-patch -p0 < $scriptDir/05_underscore.patch
+patch -p0 $patch_opts < $scriptDir/05_underscore.patch
 echo "Applying warning patch"
-patch -p0 < $scriptDir/06_warning.patch
+patch -p0 $patch_opts < $scriptDir/06_warning.patch
