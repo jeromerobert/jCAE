4df2de542f57457ac5a74bf878ed997db62fc2aa
barbier <barbier>
2008-11-19 17:25:47 +0000

    Initial version for smoothing on a background mesh
    This is a preliminary version, it does not work yet.
    
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2553 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/amibe/algos3d/SmoothNodes3DBg.java
@@ -0,0 +1,426 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finite element mesher, Plugin architecture.
+
+    Copyright (C) 2008, by EADS France
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jcae.mesh.amibe.algos3d;
+
+import org.jcae.mesh.amibe.ds.Mesh;
+import org.jcae.mesh.amibe.ds.Triangle;
+import org.jcae.mesh.amibe.ds.AbstractHalfEdge;
+import org.jcae.mesh.amibe.ds.Vertex;
+import org.jcae.mesh.amibe.util.QSortedTree;
+import org.jcae.mesh.amibe.util.PAVLSortedTree;
+import org.jcae.mesh.xmldata.MeshReader;
+import org.jcae.mesh.xmldata.MeshWriter;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Collection;
+import java.util.Iterator;
+import java.io.IOException;
+import gnu.trove.TObjectDoubleHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jcae.mesh.amibe.projection.MeshLiaison;
+
+/**
+ * Node smoothing.  Triangle quality is computed for all triangles,
+ * and vertex quality is the lowest value of its incident triangles.
+ * Vertices are sorted according to their quality, and processed
+ * iteratively by beginning with worst vertex.  A modified Laplacian
+ * smoothing is performed, as briefly explained in
+ * <a href="http://www.ann.jussieu.fr/~frey/publications/ijnme4198.pdf">Adaptive Triangular-Quadrilateral Mesh Generation</a>, by Houman Borouchaky and
+ * Pascal J. Frey.
+ * If final position improves vertex quality, point is moved.
+ */
+public class SmoothNodes3DBg
+{
+	private final static Logger LOGGER = Logger.getLogger(SmoothNodes3DBg.class.getName());
+	private final Mesh mesh;
+	private final MeshLiaison liaison;
+	private double sizeTarget = -1.0;
+	private int nloop = 10;
+	private double tolerance = Double.MAX_VALUE / 2.0;
+	private boolean preserveBoundaries = false;
+	private int progressBarStatus = 10000;
+	private static final double scaleFactor = 12.0 * Math.sqrt(3.0);
+	private double relaxation = 0.6;
+	private QSortedTree<Vertex> tree = new PAVLSortedTree<Vertex>();
+	private boolean refresh = false;
+	int processed = 0;
+	int notProcessed = 0;
+	TObjectDoubleHashMap<Triangle> qualityMap;
+	Collection<Vertex> nodeset;
+	
+	/**
+	 * Creates a <code>SmoothNodes3DBg</code> instance.
+	 *
+	 * @param bgMesh  the <code>Mesh</code> instance to refine.
+	 */
+	public SmoothNodes3DBg(Mesh m)
+	{
+		this(m, new HashMap<String, String>());
+	}
+	
+	/**
+	 * Creates a <code>SmoothNodes3DBg</code> instance.
+	 *
+	 * @param bgMesh  the <code>Mesh</code> instance to refine.
+	 * @param options  map containing key-value pairs to modify algorithm
+	 *        behaviour.  Valid keys are <code>size</code>,
+	 *        <code>iterations</code>, <code>boundaries</code>,
+	 *        <code>tolerance</code>, <code>refresh</code> and
+	 *        <code>relaxation</code>.
+	 */
+	public SmoothNodes3DBg(final Mesh bgMesh, final Map<String, String> options)
+	{
+		liaison = new MeshLiaison(bgMesh);
+		mesh = liaison.getMesh();
+		for (final Map.Entry<String, String> opt: options.entrySet())
+		{
+			final String key = opt.getKey();
+			final String val = opt.getValue();
+			if (key.equals("size"))
+				sizeTarget = Double.valueOf(val).doubleValue();
+			else if (key.equals("iterations"))
+				nloop = Integer.valueOf(val).intValue();
+			else if (key.equals("boundaries"))
+				preserveBoundaries = Boolean.valueOf(val).booleanValue();
+			else if (key.equals("tolerance"))
+				tolerance = Double.valueOf(val).doubleValue();
+			else if (key.equals("refresh"))
+				refresh = true;
+			else if (key.equals("relaxation"))
+				relaxation = Double.valueOf(val).doubleValue();
+			else
+				throw new RuntimeException("Unknown option: "+key);
+		}
+		if (LOGGER.isLoggable(Level.FINE))
+		{
+			if (sizeTarget > 0.0)
+				LOGGER.fine("Size: "+sizeTarget);
+			LOGGER.fine("Iterations: "+nloop);
+			LOGGER.fine("Refresh: "+refresh);
+			LOGGER.fine("Relaxation: "+relaxation);
+			LOGGER.fine("Tolerance: "+tolerance);
+			LOGGER.fine("Preserve boundaries: "+preserveBoundaries);
+		}
+	}
+	
+	public Mesh getOutputMesh()
+	{
+		return mesh;
+	}
+	
+	public void setProgressBarStatus(int n)
+	{
+		progressBarStatus = n;
+	}
+
+	/**
+	 * Moves all nodes until all iterations are done.
+	 */
+	private void computeTriangleQuality()
+	{
+ 		AbstractHalfEdge ot = null;
+		for (Triangle f: mesh.getTriangles())
+		{
+			if (f.hasAttributes(AbstractHalfEdge.OUTER))
+				continue;
+			ot = f.getAbstractHalfEdge(ot);
+			double val = triangleQuality(ot);
+			qualityMap.put(f, val);
+		}
+	}
+
+	public void compute()
+	{
+		LOGGER.info("Run "+getClass().getName());
+		if (nloop > 0)
+		{
+			// First compute triangle quality
+			qualityMap = new TObjectDoubleHashMap<Triangle>(mesh.getTriangles().size());
+			computeTriangleQuality();
+	
+			nodeset = mesh.getNodes();
+			if (nodeset == null)
+			{
+				nodeset = new LinkedHashSet<Vertex>(mesh.getTriangles().size() / 2);
+				for (Triangle f: mesh.getTriangles())
+				{
+					if (f.hasAttributes(AbstractHalfEdge.OUTER))
+						continue;
+					for (Vertex v: f.vertex)
+						nodeset.add(v);
+				}
+			}
+			for (int i = 0; i < nloop; i++)
+				processAllNodes();
+		}
+		LOGGER.info("Number of moved points: "+processed);
+		LOGGER.info("Total number of points not moved during processing: "+notProcessed);
+	}
+	
+	/*
+	 * Moves all nodes using a modified Laplacian smoothing.
+	 */
+	private void processAllNodes()
+	{
+ 		AbstractHalfEdge ot = null;
+		// Compute vertex quality
+		tree.clear();
+		for (Vertex v: nodeset)
+		{
+			if (!v.isManifold() || !v.isMutable())
+				continue;
+			Triangle f = (Triangle) v.getLink();
+			ot = f.getAbstractHalfEdge(ot);
+			if (ot.destination() == v)
+				ot = ot.next();
+			else if (ot.apex() == v)
+				ot = ot.prev();
+			assert ot.origin() == v;
+			double qv = vertexQuality(ot);
+			if (qv <= tolerance)
+				tree.insert(v, qv);
+		}
+		// Now smooth nodes iteratively
+		while (!tree.isEmpty())
+		{
+			Iterator<QSortedTree.Node<Vertex>> itt = tree.iterator();
+			QSortedTree.Node<Vertex> q = itt.next();
+			if (q.getValue() > tolerance)
+				break;
+			Vertex v = q.getData();
+			tree.remove(v);
+			if (v.getRef() != 0 && preserveBoundaries)
+			{
+				notProcessed++;
+				continue;
+			}
+			if (smoothNode(v, ot, q.getValue()))
+			{
+				processed++;
+				if (processed > 0 && (processed % progressBarStatus) == 0)
+					LOGGER.info("Vertices processed: "+processed);
+				if (!refresh)
+					continue;
+				assert ot != null;
+				// Update triangle quality
+				Vertex d = ot.destination();
+				do
+				{
+					ot = ot.nextOriginLoop();
+					if (ot.hasAttributes(AbstractHalfEdge.OUTER))
+						continue;
+					double qt = triangleQuality(ot);
+					qualityMap.put(ot.getTri(), qt);
+				}
+				while (ot.destination() != d);
+				// Update neighbor vertex quality
+				do
+				{
+					ot = ot.nextOriginLoop();
+					Vertex n = ot.destination();
+					if (n == mesh.outerVertex || !tree.contains(n))
+						continue;
+					if (ot.hasAttributes(AbstractHalfEdge.OUTER))
+						continue;
+					ot = ot.next();
+					double qv = vertexQuality(ot);
+					ot = ot.prev();
+					if (qv <= tolerance)
+						tree.update(n, qv);
+					else
+					{
+						tree.remove(n);
+						notProcessed++;
+					}
+				}
+				while (ot.destination() != d);
+			}
+			else
+				notProcessed++;
+		}
+	}
+	
+	private boolean smoothNode(Vertex n, AbstractHalfEdge ot, double quality)
+	{
+		Triangle f = (Triangle) n.getLink();
+ 		ot = f.getAbstractHalfEdge(ot);
+		if (ot.destination() == n)
+			ot = ot.next();
+		else if (ot.apex() == n)
+			ot = ot.prev();
+		assert ot.origin() == n;
+		double [] oldp3 = n.getUV();
+		
+		//  Compute 3D coordinates centroid
+		int nn = 0;
+		double [] centroid3 = new double[3];
+		assert n.isManifold();
+		Vertex d = ot.destination();
+		do
+		{
+			ot = ot.nextOriginLoop();
+			Vertex v = ot.destination();
+			if (v != mesh.outerVertex)
+			{
+				nn++;
+				double l = n.distance3D(v);
+				double[] newp3 = v.getUV();
+				if (sizeTarget > 0.0)
+				{
+					if (l > 1.0)
+					{
+						// Find the point on this edge which has the
+						// desired length
+						l = sizeTarget / l;
+						for (int i = 0; i < 3; i++)
+							centroid3[i] += newp3[i] + l * (oldp3[i] - newp3[i]);
+					}
+					else
+					{
+						for (int i = 0; i < 3; i++)
+							centroid3[i] += newp3[i];
+					}
+				}
+				else
+				{
+					for (int i = 0; i < 3; i++)
+						centroid3[i] += newp3[i];
+				}
+			}
+		}
+		while (ot.destination() != d);
+		assert (nn > 0);
+		for (int i = 0; i < 3; i++)
+			centroid3[i] /= nn;
+		for (int i = 0; i < 3; i++)
+			centroid3[i] = oldp3[i] + relaxation * (centroid3[i] - oldp3[i]);
+		if (!ot.checkNewRingNormals(centroid3))
+			return false;
+
+		double saveX = oldp3[0];
+		double saveY = oldp3[1];
+		double saveZ = oldp3[2];
+		liaison.move(n, centroid3);
+		// Check that quality has not been degraded
+		if (vertexQuality(ot) < quality)
+		{
+			n.moveTo(saveX, saveY, saveZ);
+			return false;
+		}
+		return true;
+	}
+
+	private double triangleQuality(AbstractHalfEdge edge)
+	{
+		Triangle f = edge.getTri();
+		assert f.vertex[0] != mesh.outerVertex && f.vertex[1] != mesh.outerVertex && f.vertex[2] != mesh.outerVertex : f;
+		double p = f.vertex[0].distance3D(f.vertex[1]) + f.vertex[1].distance3D(f.vertex[2]) + f.vertex[2].distance3D(f.vertex[0]);
+		double area = edge.area();
+		double ret = scaleFactor * area / p / p;
+		assert ret >= 0.0 && ret <= 1.01;
+		return ret;
+	}
+
+	private double vertexQuality(AbstractHalfEdge edge)
+	{
+		Vertex d = edge.destination();
+		double ret = Double.MAX_VALUE;
+		do
+		{
+			edge = edge.nextOriginLoop();
+			if (edge.hasAttributes(AbstractHalfEdge.OUTER))
+				continue;
+			double qt = triangleQuality(edge);
+			if (qt < ret)
+				ret = qt;
+		}
+		while (edge.destination() != d);
+		return ret;
+	}
+
+	private static void usage(int rc)
+	{
+		System.out.println("Usage: SmoothNodes3DBg [options] xmlDir outDir");
+		System.out.println("Options:");
+		System.out.println(" -h, --help         Display this message and exit");
+		System.out.println(" --iterations <n>   Iterate <n> times over all nodes");
+		System.out.println(" --size <s>         Set target size");
+		System.out.println(" --tolerance <t>    Consider only nodes with quality lower than <t>");
+		System.out.println(" --relaxation <r>   Set relaxation factor");
+		System.out.println(" --refresh          Update vertex quality before each iteration");
+		System.exit(rc);
+	}
+
+	/**
+	 * 
+	 * @param args [options] xmlDir outDir
+	 */
+	public static void main(String[] args)
+	{
+		org.jcae.mesh.amibe.traits.MeshTraitsBuilder mtb = org.jcae.mesh.amibe.traits.MeshTraitsBuilder.getDefault3D();
+		mtb.addNodeList();
+		Mesh mesh = new Mesh(mtb);
+		Map<String, String> opts = new HashMap<String, String>();
+		int argc = 0;
+		for (String arg: args)
+			if (arg.equals("--help") || arg.equals("-h"))
+				usage(0);
+		while (argc < args.length-1)
+		{
+			if (args[argc].length() < 2 || args[argc].charAt(0) != '-' || args[argc].charAt(1) != '-')
+				break;
+			if (args[argc].equals("--refresh") || args[argc].equals("--boundaries"))
+			{
+				opts.put(args[argc].substring(2), "true");
+				argc++;
+			}
+			else
+			{
+				opts.put(args[argc].substring(2), args[argc+1]);
+				argc += 2;
+			}
+		}
+		if (argc + 2 != args.length)
+			usage(1);
+		try
+		{
+			MeshReader.readObject3D(mesh, args[argc]);
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+		SmoothNodes3DBg smoother = new SmoothNodes3DBg(mesh, opts);
+		smoother.compute();			
+		try
+		{
+			MeshWriter.writeObject3D(smoother.getOutputMesh(), args[argc+1], null);
+		}
+		catch (IOException ex)
+		{
+			ex.printStackTrace();
+			throw new RuntimeException(ex);
+		}
+	}
+}
---
+++ amibe/src/org/jcae/mesh/amibe/projection/MeshLiaison.java
@@ -0,0 +1,289 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finite element mesher, Plugin architecture.
+
+    Copyright (C) 2008, by EADS France
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+package org.jcae.mesh.amibe.projection;
+
+import org.jcae.mesh.amibe.ds.Mesh;
+import org.jcae.mesh.amibe.ds.Vertex;
+import org.jcae.mesh.amibe.ds.Triangle;
+import org.jcae.mesh.amibe.ds.AbstractHalfEdge;
+import org.jcae.mesh.amibe.metrics.Matrix3D;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MeshLiaison
+{
+	private final static Logger LOGGER = Logger.getLogger(MeshLiaison.class.getName());
+	
+	private final Mesh backgroundMesh;
+	private final Mesh currentMesh;
+	// Local surface definition on background mesh
+	Map<Vertex, LocalSurfaceProjection> localSurface;
+	// Map between vertices of currentMesh and their projection on backgroundMesh
+	private final Map<Vertex, LocalProjection> mapCurrentVertexProjection;
+	
+	public MeshLiaison(Mesh backgroundMesh)
+	{
+		this.backgroundMesh = backgroundMesh;
+		
+		// Adjacency relations are needed on backgroundMesh
+		if (!this.backgroundMesh.hasAdjacency())
+			throw new IllegalArgumentException();
+
+		Collection<Vertex> backgroundNodeset;
+		if (this.backgroundMesh.hasNodes())
+			backgroundNodeset= this.backgroundMesh.getNodes();
+		else
+		{
+			backgroundNodeset = new LinkedHashSet<Vertex>(this.backgroundMesh.getTriangles().size() / 2);
+			for (Triangle f: this.backgroundMesh.getTriangles())
+			{
+				if (f.hasAttributes(AbstractHalfEdge.OUTER))
+					continue;
+				for (Vertex v: f.vertex)
+					backgroundNodeset.add(v);
+			}
+		}
+		
+		this.currentMesh = new Mesh();
+		// Create vertices of currentMesh
+		Map<Vertex, Vertex> mapVertexVertex = new HashMap<Vertex, Vertex>(backgroundNodeset.size()+1);
+		for (Vertex v : backgroundNodeset)
+		{
+			Vertex currentV = this.currentMesh.createVertex(v.getUV());
+			mapVertexVertex.put(v, currentV);
+			if (this.currentMesh.hasNodes())
+				this.currentMesh.add(currentV);
+		}
+		mapVertexVertex.put(this.backgroundMesh.outerVertex, this.currentMesh.outerVertex);
+
+		// Create triangles of currentMesh
+		for (Triangle t : this.backgroundMesh.getTriangles())
+		{
+			if (t.hasAttributes(AbstractHalfEdge.OUTER))
+				continue;
+			this.currentMesh.add(this.currentMesh.createTriangle(
+				mapVertexVertex.get(t.vertex[0]),
+				mapVertexVertex.get(t.vertex[1]),
+				mapVertexVertex.get(t.vertex[2])));
+		}
+		this.currentMesh.buildAdjacency();
+
+		// Compute discrete surface on backgroundMesh
+		this.localSurface = new HashMap<Vertex, LocalSurfaceProjection>(backgroundNodeset.size());
+		for (Vertex v: backgroundNodeset)
+		{
+			if (!v.isManifold() || !v.isMutable())
+				continue;
+			LocalSurfaceProjection P = new QuadricProjection(v);
+			if (!P.canProject())
+				continue;
+			this.localSurface.put(v, P);
+		}
+		
+		// Compute projections of vertices from currentMesh
+		this.mapCurrentVertexProjection = new HashMap<Vertex, LocalProjection>(backgroundNodeset.size());
+		for (Vertex v: backgroundNodeset)
+		{
+			if (!v.isManifold() || !v.isMutable())
+				continue;
+			Vertex currentV = mapVertexVertex.get(v);
+			this.mapCurrentVertexProjection.put(currentV,
+				new LocalProjection(currentV.getUV(), v.getNeighbourIteratorTriangle().next()));
+		}
+		mapVertexVertex.clear();
+	}
+	
+	public Mesh getMesh()
+	{
+		return currentMesh;
+	}
+	
+	/**
+	 * Move Vertex on the desired location and update projection map.
+	 * @param v Vertex being moved
+	 * @param target  new location
+	 * @return <code>true</code> if point has been moved, <code>false</code> otherwise.
+	 */
+	public boolean move(Vertex v, double [] target)
+	{
+		if (LOGGER.isLoggable(Level.FINER))
+			LOGGER.log(Level.FINER, "Trying to move vertex "+v+" to ("+target[0]+", "+target[1]+", "+target[2]+")");
+		Set<Triangle> visited = new HashSet<Triangle>();
+		LocalProjection proj = mapCurrentVertexProjection.get(v);
+		if (proj == null)
+			return false;
+		visited.add(proj.t);
+		int counter = 0;
+		// Coordinates of the projection of v on triangle plane
+		double [] vPlane;
+		while(true)
+		{
+			// Move v to desired location
+			v.moveTo(target[0], target[1], target[2]);
+			// Project v on surface
+			proj.quadric.project(v);
+			// Check if v crossed triangle boundary
+			vPlane = proj.projectOnTriangle(v.getUV());
+			boolean inside = proj.computeBarycentricCoordinates(vPlane);
+			
+			double [] p0 = proj.t.vertex[0].getUV();
+			double [] p1 = proj.t.vertex[1].getUV();
+			double [] p2 = proj.t.vertex[2].getUV();
+			// Constrain move within triangle boundary
+			if (!inside)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					if (proj.b[i] < 0.0)
+						proj.b[i] = 0.0;
+				}
+				// Values have been truncated
+				double invSum = 1.0 / (proj.b[0] + proj.b[1] + proj.b[2]);
+				for (int i = 0; i < 3; i++)
+					proj.b[i] *= invSum;
+				// Move vertex on boundary
+				vPlane[0] = proj.b[0]*p0[0] + proj.b[1]*p1[0] + proj.b[2]*p2[0];
+				vPlane[1] = proj.b[0]*p0[1] + proj.b[1]*p1[1] + proj.b[2]*p2[1];
+				vPlane[2] = proj.b[0]*p0[2] + proj.b[1]*p1[2] + proj.b[2]*p2[2];
+
+				AbstractHalfEdge edge = proj.t.getAbstractHalfEdge();
+				AbstractHalfEdge sym = proj.t.getAbstractHalfEdge();
+				if (proj.b[1] == 0.0)
+					edge = edge.next();
+				else if (proj.b[2] == 0.0)
+					edge = edge.prev();
+				if (LOGGER.isLoggable(Level.FINE))
+					LOGGER.log(Level.FINE, "Point is moved out of triangle by edge "+edge);
+
+				sym = edge.sym(sym);
+				if (visited.contains(sym.getTri()))
+				{
+					LOGGER.finer("Loop detected when moving vertex");
+					return false;
+				}
+				proj.updateTriangle(sym.getTri());
+				counter = 0;
+			}
+			counter++;
+			if (counter > 2)
+			{
+				LOGGER.finer("Loop in triangle detected when moving vertex");
+				return false;				
+			}
+			// Compute barycentric coordinates in the new triangle
+			proj.computeBarycentricCoordinates(vPlane);
+			proj.updateQuadric(v.getUV());
+		}
+	}
+
+	
+	private static class LocalProjection
+	{
+		LocalSurfaceProjection quadric;
+		// triangle where vertex is projected into
+		Triangle t;
+		// inverse of triangle area
+		double invArea;
+		// normal to triangle plane
+		double [] normal = new double[3];
+		// local index of origin
+		int vIndex = -1;
+		// barycentric coordinates
+		double [] b = new double[3];
+		
+		static double [] work1 = new double[3];
+		static double [] work2 = new double[3];
+		static double [] work3 = new double[3];
+		
+		public LocalProjection(double [] xyz, Triangle t)
+		{
+			updateTriangle(t);
+			computeBarycentricCoordinates(xyz);
+			updateQuadric(xyz);
+		}
+		
+		private boolean updateTriangle(Triangle newT)
+		{
+			if (newT == t)
+				return false;
+
+			t = newT;
+			invArea = 1.0 / Matrix3D.computeNormal3D(t.vertex[0].getUV(),
+				t.vertex[1].getUV(), t.vertex[2].getUV(),
+				work1, work2, normal);
+			return true;
+		}
+		
+		private boolean computeBarycentricCoordinates(double [] coord)
+		{
+			b[0] = Matrix3D.computeNormal3D(coord,
+				t.vertex[1].getUV(), t.vertex[2].getUV(),
+				work1, work2, work3) * invArea;
+			b[0] *= (work3[0]*normal[0] + work3[1]*normal[1] + work3[2]*normal[2]);
+			b[1] = Matrix3D.computeNormal3D(t.vertex[0].getUV(),
+				coord, t.vertex[2].getUV(),
+				work1, work2, work3) * invArea;
+			b[1] *= (work3[0]*normal[0] + work3[1]*normal[1] + work3[2]*normal[2]);
+			b[2] = Matrix3D.computeNormal3D(t.vertex[0].getUV(),
+				t.vertex[1].getUV(), coord,
+				work1, work2, work3) * invArea;
+			b[2] *= (work3[0]*normal[0] + work3[1]*normal[1] + work3[2]*normal[2]);
+			return b[0] >= 0.0 && b[1] >= 0.0 && b[2] >= 0.0;
+		}
+		
+		private boolean updateQuadric(double [] xyz)
+		{
+			int oldIndex = vIndex;
+			double d0 = Matrix3D.distance2(t.vertex[0].getUV(), xyz);
+			double d1 = Matrix3D.distance2(t.vertex[1].getUV(), xyz);
+			double d2 = Matrix3D.distance2(t.vertex[2].getUV(), xyz);
+			if (d0 <= d1 && d0 <= d2)
+				vIndex = 0;
+			else if (d1 <= d0 && d1 <= d2)
+				vIndex = 1;
+			else
+				vIndex = 2;
+			
+			if (vIndex == oldIndex)
+				return false;
+			quadric = new QuadricProjection(t.vertex[vIndex]);
+			return true;
+		}
+		
+		private double [] projectOnTriangle(double [] xyz)
+		{
+			double [] proj = new double[3];
+			double [] o = t.vertex[vIndex].getUV();
+			double dist =
+				(xyz[0] - o[0]) * normal[0] +
+				(xyz[1] - o[1]) * normal[1] +
+				(xyz[2] - o[2]) * normal[2];
+			for (int i = 0; i < 3; i++)
+				proj[i] = xyz[i] - dist * normal[i];
+			return proj;
+		}
+	}
+}
