8b4276b04777f5c67ff60011667c97d9b3dc85f2
barbier <barbier@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2008-11-24 10:52:22 +0000

    New org.jcae.mesh.amibe.projection.Sphere class
    This geometry will be needed to test surface projection.
    
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2556 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/src/org/jcae/mesh/amibe/projection/Sphere.java
@@ -0,0 +1,226 @@
+/* jCAE stand for Java Computer Aided Engineering. Features are : Small CAD
+   modeler, Finite element mesher, Plugin architecture.
+
+    Copyright (C) 2008, by EADS France
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA */
+
+package org.jcae.mesh.amibe.projection;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jcae.mesh.MeshOEMMIndex;
+import org.jcae.mesh.amibe.ds.Mesh;
+import org.jcae.mesh.amibe.metrics.Matrix3D;
+import org.jcae.mesh.oemm.MeshReader;
+import org.jcae.mesh.oemm.OEMM;
+import org.jcae.mesh.oemm.Storage;
+import org.jcae.mesh.xmldata.MeshWriter;
+
+/**
+ * Create a regular mesh of a sphere.  We first generate an icosahedron,
+ * this is the Platonic solid which is the best approximation of a sphere
+ * by equilateral triangles.  Then each triangle is refined and new vertices
+ * are projected onto a sphere.  Triangles are no more equilateral, but
+ * deviation is very low.  Valence of the 12 initial vertices is 5, and all
+ * other vertices have a valence of 6.
+ *
+ * @author Denis Barbier
+ */
+public class Sphere
+{
+	private final static double TAU = 0.5 * (1.0 + Math.sqrt(5.0));
+	private final static double [] ICO = new double[] {
+		   0.0,  1.0,  TAU,
+		   0.0, -1.0,  TAU,
+		   0.0,  1.0, -TAU,
+		   0.0, -1.0, -TAU,
+		   1.0,  TAU,  0.0,
+		  -1.0,  TAU,  0.0,
+		   1.0, -TAU,  0.0,
+		  -1.0, -TAU,  0.0,
+		   TAU,  0.0,  1.0,
+		  -TAU,  0.0,  1.0,
+		   TAU,  0.0, -1.0,
+		  -TAU,  0.0, -1.0
+	};
+	private final static double SCALE = 1.0 / Math.sqrt(1.0 + TAU*TAU);
+	private final static int [] FACES = new int[] {
+		 0,  1,  8,
+		 8,  1,  6,
+		 6,  1,  7,
+		 7,  1,  9,
+		 9,  1,  0,
+		
+		 2,  5,  4,
+		 4,  5,  0,
+		 0,  5,  9,
+		 9,  5, 11,
+		11,  5,  2,
+		 
+		 2,  3, 11,
+		11,  3,  7,
+		 7,  3,  6,
+		 6,  3, 10,
+		10,  3,  2,
+		 
+		 9, 11,  7,
+		 8,  4,  0,
+		 6, 10,  8,
+		 4,  8, 10,
+		 4, 10,  2
+	};
+	private final Set<UnindexedTriangle> triangles = new HashSet<UnindexedTriangle>();
+	
+	private static class UnindexedTriangle
+	{
+		double [] coord = new double[9];
+		int group;
+		UnindexedTriangle()
+		{
+		}
+
+		public UnindexedTriangle(double [] v0, double [] v1, double [] v2, int group)
+		{
+			this.group = group;
+			System.arraycopy(v0, 0, coord, 0, 3);
+			System.arraycopy(v1, 0, coord, 3, 3);
+			System.arraycopy(v2, 0, coord, 6, 3);
+		}
+		
+	}
+	
+	public void createIcosahedron()
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			UnindexedTriangle t = new UnindexedTriangle();
+			t.group = i + 1;
+			for (int j = 0; j < 3; j++)
+			{
+				int index = FACES[3*i+j];
+				t.coord[3*j]   = SCALE * ICO[3*index];
+				t.coord[3*j+1] = SCALE * ICO[3*index+1];
+				t.coord[3*j+2] = SCALE * ICO[3*index+2];
+			}
+			triangles.add(t);
+		}
+	}
+	
+	public void refine()
+	{
+		double [] x = new double[3];
+		double [][] vertex = new double[3][3];
+		double [][] middle = new double[3][3];
+		for (UnindexedTriangle t : new ArrayList<UnindexedTriangle>(triangles))
+		{
+			int last = 2;
+			for (int i = 0; i < 3; i++)
+			{
+				vertex[i][0] = t.coord[3*i];
+				vertex[i][1] = t.coord[3*i+1];
+				vertex[i][2] = t.coord[3*i+2];
+				for (int j = 0; j < 3; j++)
+					x[j] = 0.5 * (t.coord[3*i+j] + t.coord[3*last+j]);
+				double invNorm = 1.0 / Matrix3D.norm(x);
+				middle[i][0] = x[0] * invNorm;
+				middle[i][1] = x[1] * invNorm;
+				middle[i][2] = x[2] * invNorm;
+				last = i;
+			}
+			triangles.add(new UnindexedTriangle(vertex[0], middle[1], middle[0], t.group));
+			triangles.add(new UnindexedTriangle(vertex[1], middle[2], middle[1], t.group));
+			triangles.add(new UnindexedTriangle(vertex[2], middle[0], middle[2], t.group));
+			triangles.add(new UnindexedTriangle(middle[0], middle[1], middle[2], t.group));
+			triangles.remove(t);
+		}
+	}
+	
+	public boolean writeSoup(String dirname) throws FileNotFoundException, IOException
+	{
+		File dir = new File(dirname);
+		if (!dir.isDirectory())
+			dir.mkdirs();
+		DataOutputStream out = new DataOutputStream(
+			new BufferedOutputStream(new FileOutputStream(dirname+File.separator+"soup")));
+		for (UnindexedTriangle t : triangles)
+		{
+			for (int i = 0; i < 9; i++)
+				out.writeDouble(t.coord[i]);
+			out.writeInt(t.group);
+			out.writeInt(0);
+		}
+		out.close();
+		return true;
+	}
+	
+	public static void main(String [] args) throws IOException
+	{
+		// Default subdivision level
+		int level = 4;
+		// Default output directory
+		String output = "sphere";
+		
+		// Command-line parsing
+		if (args.length > 0)
+			level = Integer.parseInt(args[0]);
+		if (args.length > 1)
+			output = args[1];
+
+		Sphere sphere = new Sphere();
+		// Create icosahedron
+		sphere.createIcosahedron();
+
+		// Refine icosahedron
+		for (int i = level; i > 0; i--)
+			sphere.refine();
+		
+		// Write triangle soup and oemm into the same temporary dir
+		File tempFile = File.createTempFile("oemm", ".dir");
+		String tmpdir = tempFile.getAbsolutePath();
+		tempFile.delete();
+		File tempDir = new File(tmpdir);
+		tempDir.mkdir();
+		
+		try {
+			sphere.writeSoup(tmpdir);
+		} catch (IOException ex) {
+			Logger.getLogger(Sphere.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
+		// Build oemm
+		String [] mainArgs = new String[4];
+		mainArgs[0] = tmpdir;
+		mainArgs[1] = tmpdir;
+		mainArgs[2] = "4";
+		mainArgs[3] = "50000";
+		MeshOEMMIndex.main(mainArgs);
+		// Read oemm into a Mesh
+		OEMM oemm = Storage.readOEMMStructure(mainArgs[1]);
+		MeshReader mr = new MeshReader(oemm);
+		Mesh mesh = mr.buildWholeMesh();
+		// Write mesh onto disk
+		MeshWriter.writeObject3D(mesh, output, null);
+	}
+}
