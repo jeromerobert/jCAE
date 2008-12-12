f5da780b32a149c3393f9d57592517599513937a
barbier <barbier>
2008-12-12 12:54:48 +0000

    Add a slider
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2582 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/python/animate.py
+++ amibe/python/animate.py
@@ -6,10 +6,11 @@ from org.jcae.vtk import ColorManager, AmibeToMesh, Canvas, UNVToMesh, View, Vie
 # Swing
 from java.awt import BorderLayout, Color
 from java.awt.event import KeyEvent, KeyListener
-from javax.swing import JFrame
+from javax.swing import JFrame, JSlider
+from javax.swing.event import ChangeListener
 
 # VTK
-from vtk import vtkRenderer, vtkInteractorStyleTrackballCamera
+from vtk import vtkInteractorStyleTrackballCamera
 
 # Python
 import sys, os
@@ -25,11 +26,41 @@ if (len(args) != 1):
 index = 0
 xmlDir = args[0]
 
+def load(dir):
+	if (os.path.isdir(dir) and os.path.exists(os.path.join(dir, "jcae3d"))):
+		reader = AmibeToMesh(dir)
+		print("Loading "+dir)
+		return ViewableMesh(reader.getMesh(), MyColorManager(Color.BLUE))
+	return None
+
 class MyView(View):
+	def __init__(self, dir):
+		self.dir = dir
+		i = -1
+		while True:
+			i += 1
+			if not(os.path.isdir(dir+str(i)) and os.path.exists(os.path.join(dir+str(i), "jcae3d"))):
+				break
+		self.max = i
+		self.add(load(self.dir+"0"))
+	def getMaxIndex(self):
+		return self.max
 	def rawAdd(self, viewable):
 		self.viewables.add(viewable)
 		viewable.addCanvas(self)
 		self.setCurrentViewable(viewable)
+	def update(self, index):
+		if index > self.max:
+			index = self.max
+		if index < 0:
+			index = 0
+		oldViewable = self.getCurrentViewable()
+		newViewable = load(self.dir+str(index))
+		if (newViewable):
+			self.rawAdd(newViewable)
+			if (oldViewable):
+				self.remove(oldViewable)
+		return index
 
 class MyColorManager(ColorManager):
 	color = Color.BLACK
@@ -40,53 +71,50 @@ class MyColorManager(ColorManager):
 	def getColor(self):
 		return self.color
 
-frame = JFrame("jCAE Demo")
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-canvas = MyView()
-frame.add(canvas, BorderLayout.CENTER)
-renderer = canvas.GetRenderer()
+class SliderListener(ChangeListener):
+	def __init__(self, view):
+		self.view = view
+	def stateChanged(self, evt):
+		s = evt.getSource()
+		self.view.update(s.getValue())
 
-def load(dir):
-	if (os.path.isdir(dir) and os.path.exists(os.path.join(dir, "jcae3d"))):
-		reader = AmibeToMesh(dir)
-		print("Loading "+dir)
-		return ViewableMesh(reader.getMesh(), MyColorManager(Color.BLUE))
-	return Node
+frame = JFrame("Mesh taken from "+xmlDir+"* directories")
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+canvas = MyView(xmlDir)
+frame.add(canvas, BorderLayout.CENTER)
+
+jSlider1 = JSlider()
+jSlider1.setValue(0)
+jSlider1.setMaximum(canvas.getMaxIndex())
+jSlider1.setPaintLabels(True)
+jSlider1.addChangeListener(SliderListener(canvas))
+frame.add(jSlider1, BorderLayout.SOUTH)
 
 frame.setSize(800,600)
 frame.setVisible(True)
 
 class MyKeyListener(KeyListener):
-	def __init__(self, view, dir):
-		self.view = view
-		self.dir = dir
-		self.index = 0
-		self.viewable = load(self.dir+str(self.index))
-		self.view.add(self.viewable)
+	def __init__(self, slider):
+		self.slider = slider
 	def keyTyped(self, e):
 		pass
 	def keyReleased(self, e):
 		pass
 	def keyPressed(self, e):
 		code = int(e.getKeyCode())
-		newIndex = self.index
+		oldIndex = self.slider.getValue()
+		newIndex = oldIndex
 		if (code == KeyEvent.VK_ADD or code == KeyEvent.VK_PLUS):
-			newIndex = self.index + 1
+			newIndex = oldIndex + 1
 		elif (code == KeyEvent.VK_SUBTRACT or code == KeyEvent.VK_MINUS):
-			newIndex = self.index - 1
-			if newIndex < 0:
-				newIndex = 0
-		if newIndex != self.index:
-			newViewable = load(self.dir+str(newIndex))
-			self.view.rawAdd(newViewable)
-			self.view.remove(self.viewable)
-			self.index = newIndex
-			self.viewable = newViewable
+			newIndex = oldIndex - 1
+		if newIndex != oldIndex:
+			self.slider.setValue(newIndex)
 
-canvas.addKeyListener(MyKeyListener(canvas, xmlDir))
+canvas.addKeyListener(MyKeyListener(jSlider1))
 
 style = vtkInteractorStyleTrackballCamera()
 style.AutoAdjustCameraClippingRangeOn()
 canvas.getIren().SetInteractorStyle(style)
-renderer.ResetCamera()
+canvas.GetRenderer().ResetCamera()
 
