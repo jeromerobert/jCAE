e3b8878d1bf2368ba28da726a5079aabdab72688
barbier <barbier@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2009-04-29 08:32:37 +0000

    New patch to fix VTK #8942: vtkObjectBase.Delete() corrupts vtkGlobalJavaHash
    The Java garbage collector (called GC in this bugreport) removes weak
    references from vtkGlobalJavaHash.PointerToReference when it deletes
    unused objects.  This is all good.
    Buf if vtkObjectBase.Delete() is called explicitly (which is recommended
    in Java to avoid threading issues), vtkGlobalJavaHash.PointerToReference
    is not updated and contains an invalid entry, old vtkId points to an
    object which has been deleted from native code but not yet GC'ed.
    If native code reuses the same address before object is GC'ed,
    vtkGlobalJavaHash.PointerToReference.get(vtkId) will return this object
    instead of null.
    Here is a test case to reproduce this bug, output is:
    Exception in thread "main" java.lang.ClassCastException: vtk.vtkQuadric cannot be cast to vtk.vtkDataArray
            at vtk.vtkDataArray.CreateDataArray(vtkDataArray.java:267)
            at BugWeakRef.main(BugWeakRef.java:21)
    
    See http://www.vtk.org/Bug/view.php?id=8942
    
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2711 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ vtk-util/src/org/jcae/vtk/test/BugWeakRef.java
@@ -0,0 +1,51 @@
+/*
+ * Project Info:  http://jcae.sourceforge.net
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * (C) Copyright 2009, by EADS France
+ */
+
+package org.jcae.vtk.test;
+
+import vtk.vtkDataArray;
+import vtk.vtkDoubleArray;
+import vtk.vtkQuadric;
+import vtk.vtkSampleFunction;
+
+public class BugWeakRef
+{
+    static
+	{
+		System.out.println(System.getProperty("java.library.path"));
+		System.loadLibrary("vtkCommonJava");
+		System.loadLibrary("vtkImagingJava");
+	}
+	public static void main(String[] args)
+	{
+		vtkDataArray pseudoStatic = new vtkDoubleArray();
+		while(true)
+		{
+			pseudoStatic.CreateDataArray(8).Delete();
+
+			vtkQuadric quadric = new vtkQuadric();
+			vtkSampleFunction sample = new vtkSampleFunction();
+			sample.SetSampleDimensions(30, 30, 30);
+			sample.SetImplicitFunction(quadric);
+			sample.GetImplicitFunction();
+			quadric.Delete();
+		}
+	}
+}
