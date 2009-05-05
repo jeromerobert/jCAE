b65c5f349cb8ddca048f4049e28be4b33cf7a46a
barbier <barbier@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2009-05-05 08:25:14 +0000

    Check edge discretization
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2739 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src-test/org/jcae/mesh/bora/ds/CubeTest.java
+++ amibe/src-test/org/jcae/mesh/bora/ds/CubeTest.java
@@ -20,6 +20,7 @@
 package org.jcae.mesh.bora.ds;
 
 import org.jcae.mesh.cad.CADShapeEnum;
+import org.jcae.mesh.amibe.ds.SubMesh1D;
 
 import java.io.File;
 import java.util.Iterator;
@@ -41,34 +42,91 @@ public class CubeTest
 
 		/*
 		 *        +------+
-		 *        |  f4  |
+		 *        |  f3  |
 		 *        |      |
 		 *        |  Y=1 |
 		 * +------+------+------+------+
-		 * |  f6  |  f1  |  f5  |  f2  |
+		 * |  f5  |  f0  |  f4  |  f1  |
 		 * |      |      |      |      |
 		 * |  Z=1 |  X=1 | Z=-1 | X=-1 |
 		 * +------+------+------+------+
-		 *        |  f3  |
+		 *        |  f2  |
 		 *        |      |
 		 *        | Y=-1 |
 		 *        +------+
+		 * On each face f(i), edge length is set to k(i)=2^(1-i).
+		 * Constraints on edges are thus:
+		 *        +------+
+		 *        |  k3  |
+		 *        |k5  k4|
+		 *        |  k3  |
+		 * +------+------+------+------+
+		 * |  k5  |  k3  |  k4  |  k3  |
+		 * |k5  k5|k5  k4|k4  k4|k4  k5|
+		 * |  k5  |  k2  |  k4  |  k2  |
+		 * +------+------+------+------+
+		 *        |  k2  |
+		 *        |k5  k4|
+		 *        |  k2  |
+		 *        +------+
+		 * Shape numbers are printed on the figure below, and +/- represents
+		 * shape orientation (+ is forward, - means reversed):
+		 *            +----------+
+		 *            |    30-   |
+		 *            |          |
+		 *            |29+ 6- 27-|
+		 *            |          |
+		 *            |    28+   |
+		 * +----------+----------+----------+----------+
+		 * |   38-    |    17-   |    34+   |    21+   |
+		 * |          |          |          |          |
+		 * |37+ 8- 35-|18+ 3+ 16-|31+ 7+ 33-|20+ 4- 22-|
+		 * |          |          |          |          |
+		 * |   36+    |    15+   |    32-   |    19-   |
+		 * +----------+----------+----------+----------+
+		 *            |    24-   |
+		 *            |          |
+		 *            |25- 5+ 23+|
+		 *            |          |
+		 *            |    26+   |
+		 *            +----------+
 		 */
 		double l = 2.0;
+		BCADGraphCell[] faces = new BCADGraphCell[6];
+		int i = 0;
+		int [] e = new int[6];
 		for (Iterator<BCADGraphCell> its = root.shapesExplorer(CADShapeEnum.FACE); its.hasNext(); )
 		{
+			faces[i] = its.next();
+			e[i] = (int) (2.0 / l);
 			Hypothesis h = new Hypothesis();
 			h.setElement("T3");
 			h.setLength(l);
-			submesh.add(new Constraint(its.next(), h));
+			submesh.add(new Constraint(faces[i], h));
 
+			i++;
 			l *= 0.5;
 		}
+		int [] expected = {
+			e[2], e[4], e[3], e[5],
+			e[2], e[4], e[3], e[5],
+			e[4], e[2], e[5], e[2],
+			e[4], e[3], e[5], e[3],
+			e[4], e[4], e[4], e[4],
+			e[5], e[5], e[5], e[5]
+		};
 
 		//model.printAllHypothesis();
 		//model.printConstraints();
 		model.compute();
 		//model.printConstraints();
+
+		for (Iterator<BCADGraphCell> its = root.shapesExplorer(CADShapeEnum.EDGE); its.hasNext(); )
+		{
+			BCADGraphCell c = its.next();
+			SubMesh1D mesh1d = (SubMesh1D) c.getDiscretizations().iterator().next().getMesh();
+			assertEquals(expected[c.getId() - 15], mesh1d.getEdges().size());
+		}
 	}
 }
 
