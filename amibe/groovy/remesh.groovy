e7d9347788b368ba24f4a2dc131f2d14f72e92cb
barbier <barbier@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2009-10-06 16:35:19 +0000

    Add a groovy script to call org.jcae.mesh.amibe.algos3d.Remesh
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2861 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/groovy/remesh.groovy
@@ -0,0 +1,91 @@
+/**
+ * Remesh a mesh
+ */
+import org.jcae.mesh.amibe.ds.Mesh
+import org.jcae.mesh.amibe.algos3d.Remesh
+import org.jcae.mesh.amibe.traits.MeshTraitsBuilder
+import org.jcae.mesh.xmldata.*
+import org.apache.commons.cli.*;
+
+
+cmd=["remesh  ", "Remesh an existing mesh"]
+usage="<xmlDir>"
+
+void usage(int rc, Options options)
+{
+	HelpFormatter formatter = new HelpFormatter();
+	formatter.printHelp("amibebatch "+cmd[0].trim()+" [OPTIONS] "+usage, cmd[1], options, "");
+	System.exit(rc);
+}
+
+Options options = new Options();
+options.addOption(
+	OptionBuilder.hasArg(false)
+		.withDescription("usage information")
+		.withLongOpt("help")
+		.create('h'));
+options.addOption(
+	OptionBuilder.withArgName("VALUE").hasArg()
+		.withDescription("angle (in degrees) to detect feature edges")
+		.withLongOpt("ridgeAngle")
+		.create('a'));
+options.addOption(
+	OptionBuilder.withArgName("VALUE").hasArg()
+		.withDescription("target size")
+		.withLongOpt("size")
+		.create('t'));
+options.addOption(
+	OptionBuilder.withArgName("VALUE").hasArg()
+		.withDescription("name of a file containing metrics map")
+		.withLongOpt("metricsFile")
+		.create('m'));
+options.addOption(
+	OptionBuilder.withArgName("DIR").hasArg()
+		.withDescription("writes new mesh into this directory")
+		.withLongOpt("output")
+		.create('o'));
+
+CommandLineParser parser = new GnuParser();
+CommandLine cmd = parser.parse(options, args, true);
+if (cmd.hasOption('h'))
+	usage(0, options);
+
+String [] remaining = cmd.getArgs();
+if (remaining.length != 1)
+	usage(1, options);
+if (cmd.hasOption('t') && cmd.hasOption('m'))
+{
+	println("ERROR: -t and -m cannot be specified altogether")
+	System.exit(1);
+}
+
+String xmldir = remaining[0]
+String outDir = cmd.getOptionValue('o', xmldir)
+
+boolean setAnalytic = false
+HashMap<String, String> algoOptions = new HashMap<String, String>();
+if (cmd.hasOption('t'))
+	algoOptions.put("size", cmd.getOptionValue('t'));
+else if (cmd.hasOption('m'))
+	algoOptions.put("metricsFile", cmd.getOptionValue('m'));
+else
+	setAnalytic = true
+
+MeshTraitsBuilder mtb = MeshTraitsBuilder.getDefault3D();
+mtb.addNodeList();
+Mesh mesh = new Mesh(mtb)
+MeshReader.readObject3D(mesh, xmldir)
+Remesh algo = new Remesh(mesh, algoOptions)
+public static class RemeshMetric implements Remesh.AnalyticMetricInterface
+{
+	public double getTargetSize(double x, double y, double z)
+	{
+		return Math.min(200.0, (x - 9000.0)*(x - 9000.0) / 2250.0);
+	}
+}
+
+if (setAnalytic)
+	algo.setAnalyticMetric(new RemeshMetric());
+algo.compute();
+MeshWriter.writeObject3D(algo.getOutputMesh(), outDir, null)
+
