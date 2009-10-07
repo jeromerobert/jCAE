205d251a8c937d787b1f143dcc2c6bd1251d7e4c
barbier <barbier@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2009-10-07 07:59:44 +0000

    New script: python/remesh.py
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2863 9c4724a3-da1e-0410-bc68-88f18da35cd4

---
+++ amibe/python/remesh.py
@@ -0,0 +1,68 @@
+
+# jCAE
+from org.jcae.mesh.amibe.ds import Mesh
+from org.jcae.mesh.amibe.algos3d import Remesh
+from org.jcae.mesh.amibe.traits import MeshTraitsBuilder
+from org.jcae.mesh.xmldata import MeshReader, MeshWriter
+
+# Java
+from java.util import HashMap
+from java.lang import String
+
+# Python
+import sys
+from optparse import OptionParser
+
+"""
+Remesh an existing mesh.
+"""
+
+parser = OptionParser(usage="amibebatch remesh [OPTIONS] <inputDir>\n\nRemesh an existing mesh", prog="remesh")
+parser.add_option("-a", "--ridgeAngle", metavar="FLOAT", default=-1.0,
+                  action="store", type="float", dest="angle",
+		  help="angle (in degrees) to detect feature edges")
+parser.add_option("-t", "--size", metavar="FLOAT", default=0.0,
+                  action="store", type="float", dest="size",
+                  help="target size")
+parser.add_option("-m", "--metricsFile", metavar="STRING",
+                  action="store", type="string", dest="metricsFile",
+                  help="name of a file containing metrics map")
+parser.add_option("-o", "--output", metavar="STRING",
+                  action="store", type="string", dest="output",
+                  help="writes new mesh into this directory")
+
+(options, args) = parser.parse_args(args=sys.argv[1:])
+
+if len(args) != 1:
+	parser.print_usage()
+	sys.exit(1)
+
+xmlDir = args[0]
+outDir = options.output
+if not outDir:
+	outDir = xmlDir
+
+mtb = MeshTraitsBuilder.getDefault3D()
+mtb.addNodeList()
+mesh = Mesh(mtb)
+MeshReader.readObject3D(mesh, xmlDir)
+
+opts = HashMap()
+setAnalytic = False
+if options.size:
+	opts.put("size", str(options.size))
+elif options.metricsFile:
+	opts.put("metricsFile", options.metricsFile)
+else:
+	setAnalytic = True
+
+algo = Remesh(mesh, opts)
+class RemeshMetric(Remesh.AnalyticMetricInterface):
+	def getTargetSize(self, x, y, z):
+		return min(200.0, (x - 9000.0)*(x - 9000.0) / 2250.0)
+
+if setAnalytic:
+	algo.setAnalyticMetric(RemeshMetric());
+algo.compute();
+MeshWriter.writeObject3D(algo.getOutputMesh(), outDir, String())
+
