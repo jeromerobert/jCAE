6bfac0e5d99f8e6f543d6bcbf1c351bc8c63894c
jeromerobert <jeromerobert@9c4724a3-da1e-0410-bc68-88f18da35cd4>
2009-11-04 10:19:48 +0000

    OCCExplorer now return all TopoDS_Compounds.
    was returning only the first one.
    
    git-svn-id: https://jcae.svn.sourceforge.net/svnroot/jcae/trunk@2906 9c4724a3-da1e-0410-bc68-88f18da35cd4

--- amibe/src-occ/org/jcae/mesh/cad/occ/OCCExplorer.java
+++ amibe/src-occ/org/jcae/mesh/cad/occ/OCCExplorer.java
@@ -20,15 +20,23 @@
 
 package org.jcae.mesh.cad.occ;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.jcae.mesh.cad.CADExplorer;
 import org.jcae.mesh.cad.CADShape;
 import org.jcae.mesh.cad.CADShapeEnum;
 import org.jcae.mesh.cad.CADShapeFactory;
 import org.jcae.opencascade.jni.TopExp_Explorer;
+import org.jcae.opencascade.jni.TopoDS_Compound;
+import org.jcae.opencascade.jni.TopoDS_Iterator;
+import org.jcae.opencascade.jni.TopoDS_Shape;
 
 public class OCCExplorer implements CADExplorer
 {
 	private final TopExp_Explorer occExp;
+	private List<TopoDS_Shape> compounds;
+	private int compId;
+	
 	public OCCExplorer ()
 	{
 		occExp = new TopExp_Explorer();
@@ -37,22 +45,57 @@ public class OCCExplorer implements CADExplorer
 	public void init(CADShape s, CADShapeEnum t)
 	{
 		OCCShape shape = (OCCShape) s;
-		OCCShapeEnum type = (OCCShapeEnum) t;
-		occExp.init(shape.getShape(), type.asType());
+		if(t == CADShapeEnum.COMPOUND)
+		{
+			compId = 0;
+			compounds = new ArrayList<TopoDS_Shape>();
+			getCompounds(shape.getShape(), compounds);
+		}
+		else
+		{
+			compounds = null;
+			OCCShapeEnum type = (OCCShapeEnum) t;
+			occExp.init(shape.getShape(), type.asType());
+		}
 	}
 	
 	public boolean more()
 	{
-		return occExp.more();
+		if(compounds == null)
+			return occExp.more();
+		else
+			return compId < compounds.size();
 	}
 	
 	public void next()
 	{
-		occExp.next();
+		if(compounds == null)
+			occExp.next();
+		else
+			compId ++;
 	}
 	
 	public CADShape current()
 	{
-		return CADShapeFactory.getFactory().newShape(occExp.current());
+		TopoDS_Shape s;
+		if(compounds == null)
+			s = occExp.current();
+		else
+			s = compounds.get(compId);
+		return CADShapeFactory.getFactory().newShape(s);
+	}
+
+	private static void getCompounds(TopoDS_Shape root, List<TopoDS_Shape> result)
+	{
+		if( root instanceof TopoDS_Compound)
+		{
+			result.add(root);
+			TopoDS_Iterator it = new TopoDS_Iterator(root);
+			while(it.more())
+			{
+				getCompounds(it.value(), result);
+				it.next();
+			}
+		}
 	}
 }
