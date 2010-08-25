79f92164c27cf02e44f8bce6f4c8de0db595b8b2
Jerome Robert <jeromerobert@users.sf.net>
2010-08-25 18:23:02 +0200

    [tweakui] fix favorite tab default status

--- jcae/tweakui/src/org/jcae/netbeans/tweakui/Installer.java
+++ jcae/tweakui/src/org/jcae/netbeans/tweakui/Installer.java
@@ -15,12 +15,16 @@
  * along with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
- * (C) Copyright 2005-2009, by EADS France
+ * (C) Copyright 2005-2010, by EADS France
  */
 package org.jcae.netbeans.tweakui;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.modules.ModuleInstall;
 import org.openide.util.Exceptions;
 
@@ -33,6 +37,15 @@ public class Installer extends ModuleInstall {
 	@Override
 	public void restored() {
 		try {
+			//Show favorite tab by default
+			//layer.xml cannot be used here because the layer order cannot be
+			//controled and the one of the favorite module may override ours
+			FileObject f = FileUtil.getConfigRoot().getFileObject("Windows2/Modes/explorer/favorites.wstcref");
+			OutputStream out = f.getOutputStream();
+			InputStream in = getClass().getResourceAsStream("favoritesWstcref.xml");
+			FileUtil.copy(in, out);
+			in.close();
+			out.close();
 			Properties p = new Properties();
 			p.load(getClass().getResourceAsStream("version.properties"));
 			System.setProperty("netbeans.buildnumber", p.getProperty("version"));
