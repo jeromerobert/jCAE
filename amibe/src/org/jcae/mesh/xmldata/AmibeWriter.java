jeromerobert/jCAE

commit a237336365c10ae2ab1c3290bb1686211ff27a49

[mesh-algos] support for mesh directory without _mesh.xml file

jcae/mesh-algos/src/org/jcae/netbeans/mesh/AmibeDataObject.java
@@ -25,6 +25,8 @@ import java.beans.PropertyChangeListener;
 import java.beans.XMLEncoder;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
 import org.jcae.mesh.xmldata.Groups;
 import org.jcae.mesh.xmldata.GroupsReader;
 import org.jcae.netbeans.Utilities;
@@ -55,6 +57,25 @@ public class AmibeDataObject extends MultiDataObject implements SaveCookie
 				setModified(true);
 			}
 		});
+	}
+
+	@Override
+	public String getName() {
+		Set<Entry> se = secondaryEntries();
+		if(se.isEmpty())
+			return getPrimaryFile().getName();
+		else
+		{
+			String s = null;
+			for(Entry e:se)
+			{
+				s = e.getFile().getName();
+				if(s.endsWith("_mesh"))
+					break;
+			}
+			System.out.println("dn: "+s);
+			return s.substring(0, s.length()-"_mesh".length());
+		}
 	}
 
 	@Override

jcae/mesh-algos/src/org/jcae/netbeans/mesh/AmibeDataLoader.java
@@ -27,11 +27,9 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import org.jcae.netbeans.Utilities;
-import org.jcae.netbeans.mesh.Mesh;
 import org.openide.ErrorManager;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.loaders.DataObject;
 import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.FileEntry;
 import org.openide.loaders.MultiDataObject;
@@ -43,8 +41,7 @@ public class AmibeDataLoader extends MultiFileLoader
 {
 	public static final String REQUIRED_MIME = "text/mesh+xml";
 	private static final long serialVersionUID = 1L;
-	private static Map<FileObject, Mesh> primaryToMesh = new HashMap<FileObject, Mesh>();
-	private static Map<FileObject, FileObject> secToPrimary = new HashMap<FileObject, FileObject>();
+	private static Map<FileObject, Mesh> primaryToMesh = new HashMap<FileObject, Mesh>();	
 
 	public AmibeDataLoader()
 	{
@@ -58,33 +55,31 @@ public class AmibeDataLoader extends MultiFileLoader
 	}
 
 	@Override
-	protected FileObject findPrimaryFile(FileObject arg0) {		
+	protected FileObject findPrimaryFile(FileObject arg0) {
+		FileObject toReturn = null;
 		if(REQUIRED_MIME.equals(arg0.getMIMEType()))
 		{
-			Mesh m = primaryToMesh.get(arg0);
-			if(m == null)
-				addMapEntry(arg0);
-			return arg0;
+			toReturn = addMapEntry(arg0);
 		}
 		else if(isjCAEDirectory(arg0))
 		{
-			FileObject p = secToPrimary.get(arg0);
-			if(p == null)
-				return fillMaps(arg0);
-			return p;
+			toReturn = arg0;
 		}
 		else if("oemm".equals(arg0.getExt()) && arg0.isFolder())
 		{
-			return arg0.getParent().getFileObject(arg0.getName()+"_mesh.xml");
+			toReturn = addMapEntry(arg0.getParent().getFileObject(arg0.getName()+"_mesh.xml"));
 		}
-		return null;
+		return toReturn;
 	}
 
 	@Override
 	protected MultiDataObject createMultiObject(FileObject primaryFile)
 		throws DataObjectExistsException
 	{
-		return new AmibeDataObject(primaryFile, this, primaryToMesh.get(primaryFile));
+		Mesh m = primaryToMesh.remove(primaryFile);
+		if(m == null)
+			m = new Mesh(primaryFile.getNameExt());
+		return new AmibeDataObject(primaryFile, this, m);
 	}
 
 	@Override
@@ -95,7 +90,6 @@ public class AmibeDataLoader extends MultiFileLoader
 
 	@Override
 	protected Entry createSecondaryEntry(MultiDataObject obj, FileObject secondaryFile) {		
-		primaryToMesh.remove(secToPrimary.remove(secondaryFile));
 		return new FileEntry(obj, secondaryFile);
 	}
 
@@ -110,35 +104,24 @@ public class AmibeDataLoader extends MultiFileLoader
 		else
 			return false;
 	}
-
-	private FileObject fillMaps(FileObject arg0) {
-		for(FileObject f:arg0.getParent().getChildren())
-			if(REQUIRED_MIME.equals(f.getMIMEType()))
-				if(arg0.equals(addMapEntry(f)))
-					return f;
-		return null;
-	}
 	
 	/**
 	 *
-	 * @param arg0 primary file (.xml)
-	 * @return secondary file (amibe.dir)
+	 * @param arg0 secondary file (.xml)
+	 * @return primary file (amibe.dir)
 	 */
-	private FileObject addMapEntry(FileObject arg0)
+	private FileObject addMapEntry(FileObject secondaryFile)
 	{
-		Mesh m = createMesh(arg0);
-		primaryToMesh.put(arg0, m);
-		File fp = FileUtil.toFile(arg0.getParent());
+		File fp = FileUtil.toFile(secondaryFile.getParent());
 		if(fp == null)
 			//will happen if the FileObject is a template
 			return null;
-		String reference = fp.getPath();
-		FileObject sec = FileUtil.toFileObject(
-			new File(Utilities.absoluteFileName(m.getMeshFile(), reference)));
-		secToPrimary.put(sec, arg0);
-		return sec;
+		Mesh m = createMesh(secondaryFile);
+		FileObject primaryFile = FileUtil.toFileObject(
+			new File(Utilities.absoluteFileName(m.getMeshFile(), fp.getPath())));
+		primaryToMesh.put(primaryFile, m);
+		return primaryFile;
 	}
-
 
 	private static Mesh createMesh(FileObject file)
 	{

jcae/mesh-algos/src/org/jcae/netbeans/mesh/AmibeNode.java
@@ -59,6 +59,7 @@ public class AmibeNode extends DataNode
 	public AmibeNode(AmibeDataObject arg0)
 	{
 		super(arg0, new Children.Array());
+		setDisplayName(getName());
 		setIconBaseWithExtension("org/jcae/netbeans/mesh/amibe.png");
 	}
 
@@ -172,33 +173,6 @@ public class AmibeNode extends DataNode
 
 	public Mesh getMesh() {
 		return getCookie(AmibeDataObject.class).getMesh();
-	}
-
-	@Override
-	public String getDisplayName() {
-		return getName();
-	}
-
-	@Override
-	public void setName(String arg0)
-	{
-		try
-		{
-			String o=getName();
-			getDataObject().rename(arg0+"_mesh");
-			fireDisplayNameChange(o, arg0);
-			fireNameChange(o, arg0);
-		}
-		catch (IOException e)
-		{
-			ErrorManager.getDefault().notify(e);
-		}
-	}
-
-	public String getName()
-	{
-		String s = getDataObject().getName();
-		return s.substring(0, s.length()-"_mesh".length());
 	}
 
 	public void updateGeomNode()

commit 3f87e40aae4c9ae7d2cc0ab0140e5f762db42b5c

[core] Fix amibe mesh creation
* The template mechanism was broken by commit
a237336365c10ae2ab1c3290bb1686211ff27a49
* Support for empty amibe mesh

amibe/src/org/jcae/mesh/xmldata/AmibeWriter.java
@@ -411,9 +411,7 @@ public abstract class AmibeWriter {
 	}
 
 	private void writeSubMesh() throws IOException
-	{	
-		if(numberOfNodes == 0)
-			return;
+	{
 		try
 		{
 			XMLStreamWriter o = xmlWriter.out;

amibe/src/org/jcae/mesh/xmldata/GroupsReader.java
@@ -72,12 +72,15 @@ public class GroupsReader
 			{
 				Element triangles = (Element) xmlDoc.getElementsByTagName(
 					"triangles").item(1); /* on recupere l'element triangle */
-				Group group = new Group();
-				group.setId(0);
-				group.setName("default");
-				group.setNumberOfElements(Integer.valueOf(
-					getStringByTagName(triangles, "number")).intValue());
-				group.setOffset(0);
+				if(triangles != null)
+				{
+					Group group = new Group();
+					group.setId(0);
+					group.setName("default");
+					group.setNumberOfElements(Integer.valueOf(
+						getStringByTagName(triangles, "number")).intValue());
+					group.setOffset(0);
+				}
 			}
 			else
 			{

jcae/core/src/org/jcae/netbeans/layer.xml
@@ -440,10 +440,11 @@
                 <attr name="template" boolvalue="true"/>
                 <attr name="instantiatingIterator" newvalue="org.jcae.netbeans.mesh.bora.BoraNewWizard"/>
             </file>
-            <file name="Amibe mesh_mesh.xml">
+            <folder name="Amibe mesh">
+				<file name="jcae3d"/>
                 <attr name="template" boolvalue="true"/>
                 <attr name="instantiatingIterator" newvalue="org.jcae.netbeans.mesh.AmibeNewWizard"/>
-            </file>
+            </folder>
         </folder>
         <folder name="Privileged">
             <file name="Geometry.shadow">
@@ -451,7 +452,7 @@
                 <attr name="position" intvalue="0"/>
             </file>
             <file name="Amibe mesh.shadow">
-                <attr name="originalFile" stringvalue="Templates/jCAE/Amibe mesh_mesh.xml"/>
+                <attr name="originalFile" stringvalue="Templates/jCAE/Amibe mesh"/>
                 <attr name="position" intvalue="100"/>
             </file>
 			<file name="Bora mesh.shadow">

jcae/core/src/org/jcae/netbeans/mesh/AmibeNewWizard.java
@@ -15,19 +15,22 @@
  * along with this program; if not, write to the Free Software Foundation, Inc.,
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  *
- * (C) Copyright 2009, by EADS France
+ * (C) Copyright 2009-2010, by EADS France
  */
 
 package org.jcae.netbeans.mesh;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import javax.swing.event.ChangeListener;
+import org.jcae.mesh.xmldata.AmibeWriter;
 import org.openide.WizardDescriptor;
 import org.openide.WizardDescriptor.Panel;
 import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
 import org.openide.loaders.DataObject;
 import org.openide.loaders.TemplateWizard;
 
@@ -40,8 +43,11 @@ public class AmibeNewWizard implements WizardDescriptor.InstantiatingIterator<Wi
 	
 	public Set<AmibeDataObject> instantiate() throws IOException {
 		FileObject fo = wizard.getTargetFolder().getPrimaryFile();
-		FileObject m = fo.createData(wizard.getTargetName()+"_mesh.xml");
-		AmibeDataObject mdo = (AmibeDataObject) DataObject.find(m);
+		File f = new File(FileUtil.toFile(fo), wizard.getTargetName() + ".amibe");
+		AmibeWriter aw = new AmibeWriter.Dim3(f.getPath());
+		aw.nextSubMesh();
+		aw.finish();
+		AmibeDataObject mdo = (AmibeDataObject) DataObject.find(FileUtil.toFileObject(f));
 		mdo.save();
 		return Collections.singleton(mdo);
 	}

jcae/mesh-algos/src/org/jcae/netbeans/mesh/AmibeDataObject.java
@@ -39,6 +39,7 @@ import org.openide.loaders.DataObjectExistsException;
 import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.MultiFileLoader;
 import org.openide.nodes.Node;
+import org.openide.util.Exceptions;
 
 /**
  *
@@ -61,23 +62,42 @@ public class AmibeDataObject extends MultiDataObject implements SaveCookie
 
 	@Override
 	public String getName() {
-		Set<Entry> se = secondaryEntries();
-		if(se.isEmpty())
+		FileObject xmlMesh = getXMLMesh(false);
+		if(xmlMesh == null)
 			return getPrimaryFile().getName();
 		else
 		{
-			String s = null;
-			for(Entry e:se)
-			{
-				s = e.getFile().getName();
-				if(s.endsWith("_mesh"))
-					break;
-			}
-			System.out.println("dn: "+s);
+			String s = xmlMesh.getName();
 			return s.substring(0, s.length()-"_mesh".length());
 		}
 	}
 
+	/** Return the secondary entry which ends by _mesh.xml */
+	private FileObject getXMLMesh(boolean create)
+	{
+		FileObject toReturn = null;
+		for(Entry e:secondaryEntries())
+		{
+			String s = e.getFile().getName();
+			if(s.endsWith("_mesh"))
+				toReturn = e.getFile();
+		}
+		
+		if(create)
+		{
+			String name = getPrimaryFile().getName();
+			try {
+				FileObject parent = getPrimaryFile().getParent();
+				FileLock l = parent.lock();
+				toReturn = parent.createData(name + "_mesh.xml");
+				l.releaseLock();
+			} catch (IOException ex) {
+				Exceptions.printStackTrace(ex);
+			}
+		}
+		return toReturn;
+	}
+	
 	@Override
 	protected Node createNodeDelegate()
 	{
@@ -109,7 +129,7 @@ public class AmibeDataObject extends MultiDataObject implements SaveCookie
 		XMLEncoder encoder = null;
 		try
 		{
-			FileObject out = getPrimaryFile();
+			FileObject out = getXMLMesh(true);
 			l = out.lock();
 			encoder = new XMLEncoder(out.getOutputStream(l));
 			encoder.writeObject(mesh);