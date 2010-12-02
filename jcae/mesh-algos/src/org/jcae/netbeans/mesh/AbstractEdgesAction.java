fbbd3cb6276dc3b03ec3ffcc0343f027815a0a74
Jerome Robert <jeromerobert@users.sf.net>
2010-12-02 18:28:14 +0100

    [mesh-algos] Allow to hide free edges for the popup menu

--- jcae/mesh-algos/src/org/jcae/netbeans/mesh/AbstractEdgesAction.java
+++ jcae/mesh-algos/src/org/jcae/netbeans/mesh/AbstractEdgesAction.java
@@ -1,38 +1,136 @@
+/*
+ * Project Info:  http://jcae.sourceforge.net
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * (C) Copyright 2005-2010, by EADS France
+ */
+
+
 package org.jcae.netbeans.mesh;
 
 import java.awt.Color;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import org.jcae.mesh.xmldata.ComputeEdgesConnectivity;
 import org.jcae.netbeans.Utilities;
-import org.jcae.netbeans.mesh.AmibeDataObject;
 import org.jcae.netbeans.viewer3d.ViewManager;
 import org.jcae.vtk.AmibeOverlayProvider;
 import org.jcae.vtk.AmibeOverlayToMesh;
 import org.jcae.vtk.View;
 import org.jcae.vtk.Viewable;
-import org.openide.ErrorManager;
 import org.openide.filesystems.FileUtil;
-import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
+import org.openide.util.Lookup.Result;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
-import org.openide.util.actions.CookieAction;
+import org.openide.util.actions.BooleanStateAction;
 import org.xml.sax.SAXException;
 
-public abstract class AbstractEdgesAction extends CookieAction
+public abstract class AbstractEdgesAction extends BooleanStateAction
 {
+	private final static Logger LOGGER = Logger.getLogger(
+		AbstractEdgesAction.class.getName());
 	public abstract String getBranchGroupLabel();
 	public abstract String getActionLabel();
 	public abstract String getViewSuffix();
+	private AmibeDataObject amibeDataObject;
+	private boolean lock;
+	public AbstractEdgesAction() {
+		setBooleanState(false);
+		final Result<AmibeDataObject> r =
+			org.openide.util.Utilities.actionsGlobalContext().lookupResult(
+			AmibeDataObject.class);
+		//listen to node selection
+		r.addLookupListener(new LookupListener() {
+			public void resultChanged(LookupEvent ev) {
+				if(r.allInstances().size() == 1)
+				{
+					amibeDataObject = r.allInstances().iterator().next();
+					setEnabled(true);
+					lock = true;
+					setBooleanState(getAViewable(amibeDataObject)!=null);
+					lock = false;
+				}
+				else
+					setEnabled(false);
+			}
+		});
+
+		//
+		addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+				if(PROP_BOOLEAN_STATE.equals(evt.getPropertyName()))
+				{
+					if(lock)
+						return;
+					View view = ViewManager.getDefault().getCurrentView();
+					if((Boolean)evt.getNewValue())
+						view.add(createViewable(amibeDataObject));
+					else
+						view.remove(getAViewable(amibeDataObject));
+				}
+			}
+		});
+	}
+
+	private AViewable getAViewable(AmibeDataObject ado)
+	{
+		View view = ViewManager.getDefault().getCurrentView();
+		for(Viewable v:view.getViewables())
+		{
+			if(v instanceof AViewable)
+			{
+				AViewable av = (AViewable) v;
+				if(ado.equals(av.amibeDataObject) &&
+					getBranchGroupLabel().equals(av.getBranchGroupLabel()))
+					return av;
+			}
+		}
+		return null;
+	}
 	
-	protected void performAction(Node[] activatedNodes)
+	private class AViewable extends AmibeOverlayToMesh
+	{
+		private final AmibeDataObject amibeDataObject;
+		public AViewable(AmibeDataObject ado, String xmlDir, Color color)
+			throws ParserConfigurationException, SAXException, IOException
+		{
+			super(new AmibeOverlayProvider(
+				new File(xmlDir),
+				AbstractEdgesAction.this.getBranchGroupLabel()), color);
+			this.amibeDataObject = ado;
+		}
+
+		public String getBranchGroupLabel()
+		{
+			return AbstractEdgesAction.this.getBranchGroupLabel();
+		}
+	}
+	
+	private AViewable createViewable(AmibeDataObject c)
 	{
 		try
-		{
-			AmibeDataObject c = activatedNodes[0].getCookie(AmibeDataObject.class);
-			
+		{		
 			String reference = FileUtil.toFile(
 				c.getPrimaryFile().getParent()).getPath();
 			String xmlDir=Utilities.absoluteFileName(
@@ -49,56 +147,34 @@ public abstract class AbstractEdgesAction extends CookieAction
 				color = AmibeOverlayProvider.FREE_EDGE_COLOR;
 			else
 				color = AmibeOverlayProvider.MULTI_EDGE_COLOR;
-
-			View view = ViewManager.getDefault().getCurrentView();
-			Viewable mesh = new AmibeOverlayToMesh(new AmibeOverlayProvider(
-				new File(xmlDir), getBranchGroupLabel()), color);
-			mesh.setName(activatedNodes[0].getName()+" "+getViewSuffix());
-			view.add(mesh);
 			
-//			SelectionManager.getDefault().addInteractor(mesh, ac);
-			
-			/*				View bgView=ViewManager.getDefault().getView3D().getViewJ3D();			
-				ViewableFE fe1 = new ViewableFE(
-					new AmibeOverlayProvider(new File(xmlDir), getBranchGroupLabel()));
-				fe1.setName(activatedNodes[0].getName()+" "+getViewSuffix());
-				bgView.add(fe1);			
-				bgView.setCurrentViewable(fe1);
-			*/
+			AViewable mesh = new AViewable(c, xmlDir, color);
+			mesh.setName(c.getName()+" "+getViewSuffix());
+			return mesh;
 		}
 		catch (XPathExpressionException ex)
 		{
-			ErrorManager.getDefault().notify(ex);
+			LOGGER.log(Level.SEVERE, null, ex);
 		}
 		catch (IOException ex)
 		{
-			ErrorManager.getDefault().notify(ex);
+			LOGGER.log(Level.SEVERE, null, ex);
 		}
 		catch (ParserConfigurationException ex)
 		{
-			ErrorManager.getDefault().notify(ex);
+			LOGGER.log(Level.SEVERE, null, ex);
 		}
 		catch (SAXException ex)
 		{
-			ErrorManager.getDefault().notify(ex);
-		}						
+			LOGGER.log(Level.SEVERE, null, ex);
+		}
+		return null;
 	}
-	
-	protected int mode()
-	{
-		return CookieAction.MODE_EXACTLY_ONE;
-	}
-	
+
+	@Override
 	public String getName()
 	{
 		return NbBundle.getMessage(getClass(), getActionLabel());
-	}
-	
-	protected Class[] cookieClasses()
-	{
-		return new Class[] {
-			AmibeDataObject.class
-		};
 	}
 	
 	@Override
@@ -108,17 +184,11 @@ public abstract class AbstractEdgesAction extends CookieAction
 		// see org.openide.util.actions.SystemAction.iconResource() javadoc for more details
 		putValue("noIconInMenu", Boolean.TRUE);
 	}
-	
+
+	@Override
 	public HelpCtx getHelpCtx()
 	{
 		return HelpCtx.DEFAULT_HELP;
 	}
-	
-	@Override
-	protected boolean asynchronous()
-	{
-		return false;
-	}
-	
 }
 
