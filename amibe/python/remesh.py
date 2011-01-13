5d16be157f3473df35146467af6830ac9aae64cd
Denis Barbier <barbier@users.sf.net>
2011-01-13 12:21:46 +0100

    [Amibe] Remesh beams in remesh.py, copied from remeshBeams.py

--- amibe/python/remesh.py
+++ amibe/python/remesh.py
@@ -4,11 +4,18 @@ from org.jcae.mesh.amibe.ds import Mesh, AbstractHalfEdge
 from org.jcae.mesh.amibe.algos3d import Remesh, QEMDecimateHalfEdge, SwapEdge, PointMetric
 from org.jcae.mesh.amibe.traits import MeshTraitsBuilder
 from org.jcae.mesh.amibe.projection import MeshLiaison
+from org.jcae.mesh.amibe.metrics import EuclidianMetric3D
 from org.jcae.mesh.xmldata import MeshReader, MeshWriter
-from org.jcae.mesh.amibe.algos3d import SmoothNodes3DBg
+from org.jcae.mesh.amibe.algos3d import SmoothNodes3DBg, RemeshPolyline
+
 # Java
 from java.util import HashMap
+from java.util import ArrayList
+from java.util import LinkedHashMap
 from java.lang import String, Math
+
+# GNU trove
+from gnu.trove import TIntArrayList
 
 # Python
 import sys
@@ -110,5 +117,51 @@ opts.put("tolerance", "0.6")
 opts.put("iterations", str(8))
 SmoothNodes3DBg(liaison, opts).compute()
 
-MeshWriter.writeObject3D(liaison.mesh, outDir, "")
+#MeshWriter.writeObject3D(liaison.mesh, outDir, "")
 
+# Now remesh beams
+bgroupMap = LinkedHashMap()
+for i in xrange(liaison.mesh.getBeams().size() / 2):
+	bId = liaison.mesh.getBeamGroup(i)
+	listBeamId = bgroupMap.get(bId)
+	if listBeamId is None:
+		listBeamId = TIntArrayList(100)
+		bgroupMap.put(bId, listBeamId)
+	listBeamId.add(i)
+
+vertices = ArrayList(liaison.mesh.getBeams())
+liaison.mesh.resetBeams()
+mapGroupToListOfPolylines = LinkedHashMap()
+for bId in bgroupMap.keySet():
+	listBeamId = bgroupMap.get(bId)
+	listOfPolylines = ArrayList()
+	polyline = ArrayList()
+	lastVertex = None
+	for i in xrange(listBeamId.size()):
+		b = listBeamId.get(i) 
+		if lastVertex != vertices.get(2*b):
+			# New polyline
+			polyline = ArrayList()
+			listOfPolylines.add(polyline)
+			polyline.add(vertices.get(2*b))
+		lastVertex = vertices.get(2*b+1)
+		polyline.add(lastVertex)
+	#print "Group "+str(bId)+" contains "+str(listOfPolylines.size())+" polylines and "+str(listBeamId.size()+1)+" vertices"
+	mapGroupToListOfPolylines.put(bId, listOfPolylines)
+
+for bId in bgroupMap.keySet():
+	listBeamId = bgroupMap.get(bId)
+	listOfPolylines = mapGroupToListOfPolylines.get(bId)
+	nrPoly = listOfPolylines.size()
+	for numPoly in xrange(nrPoly):
+		polyline = listOfPolylines.get(numPoly)
+		listM = ArrayList()
+		for v in polyline:
+			listM.add(EuclidianMetric3D(options.size))
+		#print "Remesh polyline "+str(numPoly+1)+"/"+str(nrPoly)+" of group "+str(bId)+"/"+str(bgroupMap.size())+" "+str(polyline.size())+" vertices"
+		result = RemeshPolyline(liaison.mesh, polyline, listM).compute()
+		for i in xrange(result.size() - 1):
+			liaison.mesh.addBeam(result.get(i), result.get(i+1), bId)
+		#print "  New polyline: "+str(result.size())+" vertices"
+
+MeshWriter.writeObject3D(liaison.mesh, outDir, "")
