77eff9a20cac86c3c8c99f0e008ca1dd57526f94
Jerome Robert <jeromerobert@users.sf.net>
2011-08-22 19:28:53 +0200

    [vtk-util] update cmake mingw toolchain files
    * remove gcc-mingw32 as it's made obsolet by gcc-mingw-w64
    * add i686-w64-mingw32 toolchain

--- vtk-util/toolchain-i686-w64-mingw32.cmake
+++ vtk-util/toolchain-i686-w64-mingw32.cmake
@@ -1,14 +1,15 @@
-# CMake toolchain for the gcc-mingw32 Debian package
+# CMake toolchain for the 32bit compiler of the gcc-mingw-w64 Debian package
 
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i686-w64-mingw32-c++)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
 # here is the target environment located
-SET(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc /home/jerome/kitware/vtk-mingw-install )
+SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32/)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
