8e86e5968193d94f48c38fc47ee8c156bc5e2b02
Jerome Robert <jeromerobert@users.sf.net>
2012-07-17 17:49:55 +0200

    [Amibe] Fix HE.canSwapTopology
    Checking neighbour triangles is not enough.

--- amibe/src/org/jcae/mesh/amibe/ds/HalfEdge.java
+++ amibe/src/org/jcae/mesh/amibe/ds/HalfEdge.java
@@ -805,31 +805,17 @@ public class HalfEdge extends AbstractHalfEdge implements Serializable
 	}
 	
 	/**
-	 * Check that this halfedge is not in the case of DC in the following schema
-	 * <pre>
-	 * A
-	 * |\
-	 * | \D____C
-	 * | /
-	 * |/
-	 * B
-	 * </pre>
-	 * @return
+	 * Check that swapped edge does not already exists
 	 */
 	public boolean canSwapTopology()
 	{
-		//check triangles around D
-		TriangleHE t1 = next.sym.tri;
-		TriangleHE t2 = sym.prev().sym.tri;
-		boolean toReturn = t1 != t2;
-		if(toReturn)
-		{
-			//check triangles around C
-			t1 = prev().sym.tri;
-			t2 = sym.next.sym.tri;
-			toReturn = t1 != t2;
-		}
-		return toReturn;
+		Vertex a1 = apex();
+		Vertex a2 = sym.apex();
+		Iterator<Vertex> it = a1.getNeighbourIteratorVertex();
+		while(it.hasNext())
+			if(it.next() == a2)
+				return false;
+		return true;
 	}
 
 	final boolean canMoveOrigin(Mesh mesh, double [] newpt)
