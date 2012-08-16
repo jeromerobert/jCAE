127175251b104243e0355441ad0a9e20cda7bc82
Denis Barbier <barbier@users.sf.net>
2012-08-16 10:30:13 +0200

    [vtk] Drop support for VTK 5.2 and 5.4

--- vtk-util/patch/5.4/apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Script that apply all the patchs in correct order to the VTK library.
-# You must be on the root of the VTK source directory when executing this script.
-
-scriptDir=$(dirname $0)
-
-[ -L patches ] || ln -s $scriptDir patches
-quilt push -a
