b48ad54e16995a1d3c01244602b6d3f7213072ca
Jerome Robert <jeromerobert@users.sf.net>
2013-08-22 16:05:10 +0200

    [Amibe] QEMDecimateHalfEdge: fix a NullPointerException

--- amibe/src/org/jcae/mesh/amibe/algos3d/QEMDecimateHalfEdge.java
+++ amibe/src/org/jcae/mesh/amibe/algos3d/QEMDecimateHalfEdge.java
@@ -374,7 +374,8 @@ public class QEMDecimateHalfEdge extends AbstractAlgoHalfEdge
 		q3.computeQuadric3DError(q1, q2);
 		q3.optimalPlacement(v1, v2, q1, q2, placement, v3);
 		v3.setLink(null);
-		liaison.move(v3, v3, false);
+		if(liaison != null)
+			liaison.move(v3, v3, false);
 		if (!mesh.canCollapseEdge(current, v3))
 			return false;
 		if (!metrics.isEmpty())
