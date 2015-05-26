2f558b63cee63bdb52e9518c625481a797c33ece
Jerome Robert <jeromerobert@gmx.com>
2015-05-26 14:26:29 +0200

    [Amibe] AbstractLocalRemesher: Do not tag inserted edges as non manifold

--- amibe/src/org/jcae/mesh/amibe/projection/AbstractLocaleRemesher.java
+++ amibe/src/org/jcae/mesh/amibe/projection/AbstractLocaleRemesher.java
@@ -169,6 +169,7 @@ public abstract class AbstractLocaleRemesher {
 	private void updateAdjacency(Mesh mesh, Collection<AbstractHalfEdge> edges,
 		Collection<Triangle> newTriangles)
 	{
+		Collection<Triangle> newTrianglesCopy = HashFactory.createSet(newTriangles);
 		edgeMap.clear();
 		for(Triangle t: newTriangles)
 		{
@@ -205,20 +206,23 @@ public abstract class AbstractLocaleRemesher {
 			group = e.getTri().getGroupId();
 			assert edgeMap.contains(e) : e + "\n:sym" + edgeMap.get(e.sym());
 			Triangle newTri = edgeMap.remove(e).getTri();
+			newTrianglesCopy.remove(newTri);
+			//assert newTri.getGroupId() == -1: newTri;
 			newTri.setGroupId(group);
 			assert newTriangles.contains(newTri);
 			updateLink(e.origin(), e.getTri(), newTri);
 			updateLink(e.destination(), e.getTri(), newTri);
 		}
 
-		// only edges created from vertices remain in edgeMap. We set their
-		// group to the group of an input edge
-		for(AbstractHalfEdge e: edgeMap.keySet())
-		{
-			e.getTri().setGroupId(group);
-			Triangle bt = e.createBoundaryTriangle(mesh);
-			if(bt != null)
-				newTriangles.add(bt);
+		// only triangles created from inside vertices remain in newTriangleCopy.
+		// We set their group to the group of an input edge
+		for(Triangle t:newTrianglesCopy) {
+			t.setGroupId(group);
+			for(int i = 0; i < 3; i++) {
+				Vertex v = t.getV(i);
+				if(v.getLink() == null)
+					v.setLink(t);
+			}
 		}
 	}
 
